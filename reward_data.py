2e519ce0049ee984ab89c211a866463a7b47d0aa
nostalgebraist <nostalgebraist@gmail.com>
2020-07-12 11:35:44 -0700

    july sync: reward_data.py

--- reward_data.py
+++ reward_data.py
@@ -13,19 +13,10 @@ from reblogs_v5 import *
 from ratelimit_util import RateLimitClient
 from response_cache import ResponseCache, PostIdentifier, CachedResponseType
 
+from munging_shared import get_body, process_post_from_post_payload
+
 # TODO: DRY
 blogName = "nostalgebraist-autoresponder"
-
-# TODO: define this only once (copy/paste from tumbl.py)
-def _get_body(post):
-    body_keys = ["body", "answer"]
-    for k in body_keys:
-        body = post.get(k, None)
-        if body is not None:
-            break
-    if body is None:
-        print(f"couldn't find body, tried keys {body_keys}, available keys are {post.keys()}")
-    return body
 
 def _get_reblog_data_body(post):
     tree_html, comment = post['reblog']['tree_html'], post['reblog']['comment']
@@ -33,9 +24,13 @@ def _get_reblog_data_body(post):
 
 
 def process_body(post):
+    # this is buggy currently (example: post ID 621671700008976384)
+    # TODO: fix and re-generate old data
+
+    # TODO: also make this work correctly for replies
     tree_html, comment = _get_reblog_data_body(post)
     if len(tree_html) == 0:
-        body = _get_body(post)
+        body = get_body(post)
     else:
         body = tree_html
 
@@ -57,17 +52,25 @@ def _autoresponse_to_prompt_and_continuation(processed):
     return prompt, continuation
 
 
-def get_prompt_and_continuation(post):
-    processed, post_metadata = process_body(post)
-
-    if post_metadata['is_reblog'] or post_metadata['is_ask']:
+def get_prompt_and_continuation_from_processed(processed: str):
+    if A_CHAR in processed:
         prompt, continuation = _autoresponse_to_prompt_and_continuation(processed)
-    elif post_metadata['is_orig']:
-        # TODO: undo tight coupling to orig post making code
-        prompt = "endoftext|>" + ORIG_POST_CHAR
+    elif ORIG_POST_CHAR in processed:
+        prompt = ORIG_POST_CHAR
         continuation = processed[processed.index(ORIG_POST_CHAR)+1:]
     else:
         raise ValueError(f"don't know how to deal with post")
+
+    if "<|endoftext|>" in continuation:
+        continuation = continuation[:continuation.index("<|endoftext|>")+2]
+
+    return prompt, continuation
+
+
+def get_prompt_and_continuation(post):
+    processed = process_post_from_post_payload(post)
+
+    prompt, continuation = get_prompt_and_continuation_from_processed(processed)
 
     continuation += (T_CHAR + " ".join([f"#{tag}" for tag in post.get("tags", [])]) + "<|")
 
@@ -119,6 +122,8 @@ def full_notes(response_cache, post):
 def notes_since_last_reblog(response_cache, post, verbose=False):
     post_id = str(post['id'])
     notes = full_notes(response_cache, post)
+    if len(notes) == 0:
+        return notes
 
     # notes come in descending temporal order, reverse to unconfuse myself
     notes_ascending = notes[::-1]
@@ -146,9 +151,24 @@ def notes_since_last_reblog(response_cache, post, verbose=False):
 def count_notes_for_post(response_cache, post):
     return len(notes_since_last_reblog(response_cache, post))
 
+def count_slots(ids_to_reward_data):
+    n_total = len(ids_to_reward_data)
+    n_unfilled_notes = len([v for v in ids_to_reward_data.values() if v.get("note_count") is None])
+    n_unfilled_pc = len([v for v in ids_to_reward_data.values() if (v.get("prompt") is None) or (v.get("continuation") is None)])
+
+    return n_total, n_unfilled_notes, n_unfilled_pc
+
+def show_slots(ids_to_reward_data):
+    n_total, n_unfilled_notes, n_unfilled_pc = count_slots(ids_to_reward_data)
+    print(f"{n_unfilled_pc}/{n_total} need text, {n_unfilled_notes}/{n_total} need notes")
+
+def limit_to_unfilled(posts, ids_to_reward_data):
+    return [p for p in posts
+            if p['id'] in ids_to_reward_data
+            and ids_to_reward_data[p['id']]['note_count'] is None]
 
 def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=False,  ids_to_reward_data=dict(),
-                       ignore_reblogs_from_dash=True):
+                       ignore_reblogs_from_dash=True, notes_only=True, fill_only=True):
     ratelimit_client = RateLimitClient.from_tumblr_rest_client(client, blogName=blogName)
     response_cache = ResponseCache.load(client)
 
@@ -157,26 +177,32 @@ def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=Fal
 
     def _on_new_post_batch(posts, ids_to_reward_data):
         for p in posts:
-            try:
                 post_id = p['id']
-                prompt, continuation = get_prompt_and_continuation(p)
                 note_count = count_notes_for_post(response_cache, p)
 
                 if post_id in ids_to_reward_data:
                     existing_note_count = ids_to_reward_data[post_id].get("note_count")
-                    if existing_note_count != note_count:
+                    if existing_note_count != note_count and existing_note_count is not None:
                         print(f"updating note_count from {existing_note_count} to {note_count} for {post_id}")
 
-                ids_to_reward_data[post_id] = {"prompt": prompt,
-                                               "continuation": continuation,
-                                               "note_count": note_count}
+                ids_to_reward_data[post_id]["note_count"] = note_count
+            else:
+                ids_to_reward_data[post_id] = {"note_count": note_count}
 
-            except Exception as e:
-                print(f"hit {e} on {p['id']}")
+            if not notes_only:
+                prompt, continuation = get_prompt_and_continuation(p)
+                ids_to_reward_data[post_id]["prompt"] = prompt
+                ids_to_reward_data[post_id]["continuation"] = continuation
+
+        show_slots(ids_to_reward_data)
         return ids_to_reward_data
 
-    def _reward_post_screener(posts):
+    def _reward_post_screener(posts, ids_to_reward_data, fill_only=True):
         screened = [p for p in posts]
+
+        if fill_only:
+            screened = limit_to_unfilled(screened, ids_to_reward_data)
+            print(f"after limiting to unfilled: {len(screened)}")
 
         # ignore #quotes (very old behavior)
         screened = [p for p in screened if "quotes" not in p.get("tags", [])]
@@ -185,7 +211,10 @@ def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=Fal
         screened = [p for p in screened if "mood" not in p.get("caption", "")]
 
         if ignore_reblogs_from_dash:
+            print(f"before ignore_reblogs_from_dash: {len(screened)}")
+            print([p.get('source_title', blogName) for p in screened])
             screened = [p for p in screened if p.get('source_title', blogName) == blogName]
+            print(f"after ignore_reblogs_from_dash: {len(screened)}")
 
         if len(screened) < len(posts):
             print(f"Ignoring {len(posts)-len(screened)} of {len(posts)} posts")
@@ -193,10 +222,10 @@ def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=Fal
 
     # scraping routine
     count_check_requests_start = ratelimit_client.get_ratelimit_data()['day']['remaining']
-    limit_ = 15
+    limit_ = 50
     offset_ = offset
     posts = []
-    next_ = response_cache.client.posts(blogName, limit=limit_, offset=offset_)["posts"]
+    next_ = response_cache.client.posts(blogName, limit=limit_, offset=offset_, notes_info=True)['posts']
     seen_id_count = 0
     new_id_count = None
     starting_ids = set(ids_to_reward_data.keys())
@@ -208,7 +237,8 @@ def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=Fal
 
         offset_ += len(next_)
 
-        new_post_batch = _reward_post_screener(next_)
+        new_post_batch = _reward_post_screener(next_, ids_to_reward_data, fill_only=fill_only)
+        response_cache.record_response_to_cache({"posts": new_post_batch})
         ids_to_reward_data = _on_new_post_batch(new_post_batch, ids_to_reward_data)
 
         new_ids = {p['id'] for p in new_post_batch}
@@ -220,18 +250,19 @@ def scrape_note_counts(client, max_to_scrape=None, offset=0, stop_on_seen_id=Fal
             if (seen_id_count > 0) and stop_on_seen_id:
                 print(f"ids {new_ids.intersection(starting_ids)} ({seen_id_count} ids) in both new and starting sets")
 
-        count_check_requests_end = ratelimit_client.get_ratelimit_data()['day']['remaining']
+        rld=ratelimit_client.get_ratelimit_data()
+        count_check_requests_end = rld['day']['remaining']
         count_check_requests_diff = count_check_requests_start - count_check_requests_end
         print(f"used {count_check_requests_diff} requests so far")
 
-        print(ratelimit_client.get_ratelimit_data())
+        print(rld)
         time.sleep(0.33)
-        next_ = response_cache.client.posts(blogName, limit=limit_, offset=offset_)["posts"]
+        next_ = response_cache.client.posts(blogName, limit=limit_, offset=offset_, notes_info=True)['posts']
 
     return ids_to_reward_data, offset_
 
 
-def scrape_and_save(client, max_to_scrape=50, reset_offset=False, save_path="reward/reward.pkl.gz", ignore_reblogs_from_dash=True):
+def scrape_and_save(client, max_to_scrape=50, reset_offset=False, save_path="reward/reward.pkl.gz", ignore_reblogs_from_dash=True, notes_only=True, fill_only=True):
     if not os.path.exists(save_path):
         ids_to_reward_data = {}
         offset = 0
@@ -245,7 +276,8 @@ def scrape_and_save(client, max_to_scrape=50, reset_offset=False, save_path="rew
         offset = 0
 
     print(f"starting with {len(ids_to_reward_data)} posts of data")
-    ids_to_reward_data, offset = scrape_note_counts(client, max_to_scrape, offset, ids_to_reward_data=ids_to_reward_data, ignore_reblogs_from_dash=ignore_reblogs_from_dash)
+    ids_to_reward_data, offset = scrape_note_counts(client, max_to_scrape, offset, ids_to_reward_data=ids_to_reward_data, ignore_reblogs_from_dash=ignore_reblogs_from_dash,
+                                                    notes_only=notes_only, fill_only=fill_only)
 
     print(f"ids_to_reward_data: {len(ids_to_reward_data)} items")
     print(f"offset: {offset}")
@@ -256,17 +288,19 @@ def scrape_and_save(client, max_to_scrape=50, reset_offset=False, save_path="rew
     print(f"saved {len(ids_to_reward_data)} posts of data")
 
 
-def scrape_new_and_save(client, save_path="reward/reward.pkl.gz", offset=0, max_to_scrape=None, stop_on_seen_id=True, dry_run=False, ignore_reblogs_from_dash=True):
+def scrape_new_and_save(client, save_path="reward/reward.pkl.gz", offset=0, max_to_scrape=50, stop_on_seen_id=False, dry_run=False, ignore_reblogs_from_dash=True, notes_only=True, fill_only=True):
     if not os.path.exists(save_path):
         ids_to_reward_data = {}
     else:
         with open(save_path, "rb") as f:
             data = pickle.load(f)
         ids_to_reward_data = data["ids_to_reward_data"]
+        show_slots(ids_to_reward_data)
 
-    print(f"starting with {len(ids_to_reward_data)} posts of data\n")
+    n_total, n_unfilled_notes, n_unfilled_pc = count_slots(ids_to_reward_data)
+    print(f"starting with {n_total-n_unfilled_notes} posts of data\n")
 
-    ids_to_reward_data, offset = scrape_note_counts(client, max_to_scrape=max_to_scrape, offset=offset, stop_on_seen_id=stop_on_seen_id, ids_to_reward_data=ids_to_reward_data, ignore_reblogs_from_dash=ignore_reblogs_from_dash)
+    ids_to_reward_data, offset = scrape_note_counts(client, max_to_scrape=max_to_scrape, offset=offset, stop_on_seen_id=stop_on_seen_id, ids_to_reward_data=ids_to_reward_data, ignore_reblogs_from_dash=ignore_reblogs_from_dash, notes_only=notes_only, fill_only=fill_only)
 
     print(f"ids_to_reward_data: {len(ids_to_reward_data)} items")
     print(f"offset: {offset}")
@@ -275,4 +309,5 @@ def scrape_new_and_save(client, save_path="reward/reward.pkl.gz", offset=0, max_
         data = {"ids_to_reward_data": ids_to_reward_data, "offset": offset}
         with open(save_path, "wb") as f:
             pickle.dump(data, f)
-        print(f"saved {len(ids_to_reward_data)} posts of data")
+        n_total, n_unfilled_notes, n_unfilled_pc = count_slots(ids_to_reward_data)
+        print(f"saved {n_total-n_unfilled_notes} posts of data")

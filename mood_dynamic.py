9c547b4a2853aa2f4e6a6e4bd11ed22a3dfab180
nostalgebraist <nostalgebraist@gmail.com>
2021-02-03 14:17:17 -0800

    feb sync: mood_dynamic.py

--- mood_dynamic.py
+++ mood_dynamic.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -11,12 +12,17 @@ register_matplotlib_converters()
 from scipy.signal import lsim, lti, StateSpace
 from scipy.signal.ltisys import StateSpaceContinuous
 
+from tqdm.autonotebook import tqdm
+
 from response_cache import ResponseCache, UserInputType, PostIdentifier, ReplyIdentifier, CachedResponseType
 from mood import get_mood_by_name, random_mood_at_pst_datetime, logit_diff_to_pos_sent, pos_sent_to_logit_diff
 
 STEP_SEC = 30 * 1
 TAU_SEC = 3600 * 12
 TAU_SEC_2ND = 60 * 60
+
+WEIGHTED_AVG_START_TIME = pd.Timestamp('2021-01-04 09:10:00')
+WEIGHTED_AVG_P75_WEIGHT = 0.5
 
 RESPONSE_SCALE_BASE = 0.15 #0.1 # 0.2 #0.5
 DETERMINER_CENTER = -3.1 #-2.4 # -1.5 #-2
@@ -27,12 +33,32 @@ DETERMINER_CENTER_UPDATES = {pd.Timestamp('2020-08-20 01:00:00'): -2.4,
                              pd.Timestamp('2020-10-28 17:00:00'): -2.4,
                              pd.Timestamp('2020-11-04 11:00:00'): -2.78,
                              pd.Timestamp('2020-11-13 19:00:00'): -2.7,
-                             pd.Timestamp('2020-11-15 07:30:00'): -2.6,}
+                             pd.Timestamp('2020-11-15 07:30:00'): -2.6,
+                             pd.Timestamp('2020-12-04 07:00:00'): -2.5,
+                             pd.Timestamp('2020-12-10 08:35:00'): -2.35,
+                             pd.Timestamp('2020-12-10 23:45:00'): -2.,
+                             pd.Timestamp('2020-12-18 15:35:00'): -2.2,
+                             pd.Timestamp('2020-12-21 15:25:00'): -2.3,
+                             WEIGHTED_AVG_START_TIME: 0.,
+                             pd.Timestamp('2020-01-20 18:20:00'): 0.2,
+                             pd.Timestamp('2020-01-29 08:35:00'): 0.1,
+                             pd.Timestamp('2020-01-30 14:25:00'): 0.,
+                             }
 DETERMINER_MULTIPLIER_UPDATES = {pd.Timestamp('2020-08-25 17:00:00'): 0.1/RESPONSE_SCALE_BASE,
                                  pd.Timestamp('2020-10-21 21:15:00'): 0.075/RESPONSE_SCALE_BASE,
                                  pd.Timestamp('2020-11-16 10:45:00'): 0.0667/RESPONSE_SCALE_BASE,
                                  pd.Timestamp('2020-11-25 11:30:00'): 0.1/RESPONSE_SCALE_BASE,
                                  pd.Timestamp('2020-11-27 08:55:00'): 0.15/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2020-12-04 07:00:00'): 0.1/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2020-12-09 19:50:00'): 0.075/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2020-12-20 23:30:00'): 0.05/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-08 08:55:00'): 0.075/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-08 09:10:00'): 0.1/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-13 09:20:00'): 0.15/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-14 08:00:00'): 0.2/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-14 20:35:00'): 0.15/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2021-01-20 07:40:00'): 0.1/RESPONSE_SCALE_BASE,
+                                 pd.Timestamp('2020-01-29 08:35:00'): 0.125/RESPONSE_SCALE_BASE,
                                  }
 
 MOOD_NAME_TO_DYNAMIC_MOOD_VALUE_MAP = {
@@ -49,6 +75,7 @@ _ordered_cutoffs_moods = [(c, [k for k, v in MOOD_NAME_TO_DYNAMIC_MOOD_VALUE_MAP
                           for c in _ordered_cutoffs]
 
 GENERATED_TS_FIRST_STABLE = pd.Timestamp('2020-05-26 19:00:00')
+DUPLICATES_BUGFIX_START_TS = pd.Timestamp('2020-12-19 11:00:00')
 
 WINDOW_LENGTH_DAYS = 2.5
 
@@ -165,7 +192,9 @@ class DynamicMoodSystem:
         return determiner_multiplier_s
 
 
-def compute_dynamic_mood_inputs(response_cache: ResponseCache) -> pd.DataFrame:
+def compute_dynamic_mood_inputs(response_cache: ResponseCache,
+                                weighted_avg_start_time: pd.Timestamp=WEIGHTED_AVG_START_TIME,
+                                ) -> pd.DataFrame:
     df = pd.DataFrame.from_records(
     [{"timestamp": ident.timestamp,
       "blog_name": ident.blog_name,
@@ -182,6 +211,11 @@ def compute_dynamic_mood_inputs(response_cache: ResponseCache) -> pd.DataFrame:
     ]
     ).drop_duplicates(subset=["timestamp"])
 
+    _filter = df.generated_logit_diff.notnull()
+    df.loc[_filter, "p75_generated_logit_diff"] = df.loc[_filter, "generated_logit_diff"].apply(
+        lambda l: np.percentile(l, 75)
+    )
+
     df["time"] = df.timestamp.apply(lambda ts: datetime.fromtimestamp(ts))
     _filter = df.generated_ts.notnull()
     df.loc[_filter, "time"] = df.loc[_filter, "generated_ts"]
@@ -192,19 +226,37 @@ def compute_dynamic_mood_inputs(response_cache: ResponseCache) -> pd.DataFrame:
         print(f"keeping {sum(_filter)} of {len(df)} rows")
     df = df[_filter]
 
-    _filter = df.generated_logit_diff.notnull()
-    df.loc[_filter, "p75_generated_logit_diff"] = df.loc[_filter, "generated_logit_diff"].apply(
-        lambda l: np.percentile(l, 75)
-    )
+    df['using_weighted_avg'] = df["time"]>=weighted_avg_start_time
 
-    df["determiner"] = df.apply(
-        lambda row: convert_p75_generated_logit_diff_to_user_input_logit_diff(row.p75_generated_logit_diff)
-        if (np.isfinite(row.p75_generated_logit_diff) and (row.generated_ts == row.generated_ts))
-        else row.logit_diff,
-        axis=1
-    )
+    def _compute_determiner(row):
+        if np.isfinite(row.p75_generated_logit_diff) and (row.generated_ts == row.generated_ts):
+            if row.using_weighted_avg:
+                weighted_avg = ((1-WEIGHTED_AVG_P75_WEIGHT) * row.logit_diff) +\
+                (WEIGHTED_AVG_P75_WEIGHT * row.p75_generated_logit_diff)
+
+                # one time empirical lr fit, see `sentiment_refresh_2021.ipynb`
+                weighted_avg_fitted = (0.61029747 * weighted_avg) + 0.4252486735525668
+
+                return weighted_avg_fitted
+            else:
+                return convert_p75_generated_logit_diff_to_user_input_logit_diff(row.p75_generated_logit_diff)
+        else:
+            return row.logit_diff
+
+    df["determiner"] = df.apply(_compute_determiner, axis=1)
+
+    # df["determiner"] = df.apply(
+    #     lambda row: convert_p75_generated_logit_diff_to_user_input_logit_diff(row.p75_generated_logit_diff)
+    #     if (np.isfinite(row.p75_generated_logit_diff) and (row.generated_ts == row.generated_ts))
+    #     else row.logit_diff,
+    #     axis=1
+    # )
 
     mood_inputs = df.set_index("time")
+    duplicate_bug_filter = ~mood_inputs.index.duplicated(keep="first")
+    duplicate_bug_filter = duplicate_bug_filter | (mood_inputs.index < DUPLICATES_BUGFIX_START_TS)
+    mood_inputs = mood_inputs[duplicate_bug_filter]
+
     return mood_inputs
 
 
@@ -308,7 +360,7 @@ def compute_dynamic_moodspec_at_time(response_cache: ResponseCache,
                                      window_length_days: float=None,  # pass None for unbounded
                                      system: DynamicMoodSystem=None,
                                      use_p75: bool=True,
-                                     verbose: bool=True
+                                     verbose: bool=True,
                                      ) -> dict:
     mood_inputs = compute_dynamic_mood_inputs(response_cache)
     mood_value = compute_dynamic_mood_at_time(mood_inputs, time, window_length_days, use_p75=use_p75)
@@ -323,7 +375,9 @@ def create_mood_graph(response_cache: ResponseCache,
                       window_length_days: float=WINDOW_LENGTH_DAYS,
                       system: DynamicMoodSystem=None,
                       in_logit_diff_space: bool=True,
-                      save_image: bool=True) -> str:
+                      font: str="Menlo",
+                      save_image: bool=True,
+                      show_image: bool=False,) -> str:
     ytrans = pos_sent_to_logit_diff if in_logit_diff_space else lambda x: x
 
     mood_inputs = compute_dynamic_mood_inputs(response_cache)
@@ -353,8 +407,23 @@ def create_mood_graph(response_cache: ResponseCache,
         plt.ylim(min(default_bottom, lti_series.min()), max(default_top, lti_series.max()))
 
     plt.legend(fontsize=16, );
+
     plt.tick_params(labelsize=16)
     plt.tick_params(axis='x', labelrotation=80)
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%-I %p'))
+    plt.tick_params(axis='x', labelrotation=70)
+
+    plt.grid(axis='x')
+
+    ax=plt.gca()
+    for t in ax.get_xticklabels():
+        t.set_fontname(font)
+    for t in ax.get_yticklabels():
+        t.set_fontname(font)
+    for t in ax.legend_.texts:
+        t.set_fontname(font)
+
 
     if save_image:
         image_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".png"
@@ -362,4 +431,100 @@ def create_mood_graph(response_cache: ResponseCache,
         plt.savefig(path, bbox_inches='tight')
         plt.close(fig)
         return path
+    if show_image:
+        plt.show()
+    else:
     plt.close(fig)
+
+
+def counterfactual_mood_graph(mood_inputs,
+                              determiner_centers,
+                              determiner_multipliers=None,
+                              n_days=1,
+                              start_time: datetime=None,
+                              end_time: datetime=None,
+                              window_length_days: float=WINDOW_LENGTH_DAYS,
+                              in_logit_diff_space: bool=True,) -> str:
+    ytrans = pos_sent_to_logit_diff if in_logit_diff_space else lambda x: x
+    if end_time is None:
+        end_time = datetime.now()
+    if start_time is None:
+        start_time = end_time - pd.Timedelta(days=n_days)
+
+    systems = {"actual": DynamicMoodSystem()}
+    left_time = start_time-pd.Timedelta(days=window_length_days)
+
+    for dc in determiner_centers:
+        if dc is not None:
+            new_dc_updates = {k: v for k, v in DETERMINER_CENTER_UPDATES.items() if k < left_time}
+            new_dc_updates[left_time] = dc
+            systems[f"dc={dc:.2f}"] = DynamicMoodSystem(determiner_center_updates=new_dc_updates)
+
+        if determiner_multipliers is not None:
+            for dm in determiner_multipliers:
+                new_dm_updates = {k: v for k, v in DETERMINER_MULTIPLIER_UPDATES.items() if k < left_time}
+                new_dm_updates[left_time] = dm
+                dm_s = f"{dm*RESPONSE_SCALE_BASE:.3f}x"
+                if dc is not None:
+                    systems[f"dc={dc:.2f}, dm={dm_s}"] = DynamicMoodSystem(
+                        determiner_center_updates=new_dc_updates,
+                        determiner_multiplier_updates=new_dm_updates,
+                        )
+                if f"dm={dm_s}" not in systems:
+                    systems[f"dm={dm_s}"] = DynamicMoodSystem(determiner_multiplier_updates=new_dm_updates)
+
+    lti_serieses = {}
+    for name, system in tqdm(systems.items()):
+        lti_series = compute_dynamic_mood_over_interval(mood_inputs, left_time, end_time, system).apply(ytrans)
+        lti_series = lti_series.loc[start_time:end_time]
+        lti_serieses[name] = lti_series
+
+    fig = plt.figure(figsize=(8, 6))
+    tops, bottoms = [], []
+    for name, lti_series in lti_serieses.items():
+        tops.append(lti_series.max())
+        bottoms.append(lti_series.min())
+        ls = '-'
+        if 'dc=' in name and 'dm=' in name:
+            ls = '-.'
+        elif 'dm=' in name:
+            ls = '--'
+        alpha = 1 if 'actual' in name else 0.667
+        plt.plot(lti_series.index, lti_series.values, label=name, ls=ls, alpha=alpha)
+
+    print(tops)
+    print(bottoms)
+
+    colors = {"only_happy": "#000080",
+              "only_non_sad": "#8888FF",
+              "only_non_happy": "#FF6666",
+              "only_sad": "#800000",
+              }
+    display_names = {"only_sad": ":(",
+                     "only_non_happy": ":|",
+                     "only_non_sad": ":)",
+                     "only_happy": ":D"}
+
+    for k in ["only_happy", "only_non_sad", "only_non_happy", "only_sad"]:
+        plt.axhline(ytrans(MOOD_NAME_TO_DYNAMIC_MOOD_VALUE_MAP[k]),
+                    # label=display_names[k],
+                    ls='--', c=colors[k])
+
+    golives = {pd.Timestamp('2020-11-22'): 'v9_golive_approx',
+               pd.Timestamp('2020-12-05'): 'v9_1_golive_approx',
+               pd.Timestamp('2020-12-07'): 'v9_1R2_golive_approx'}
+    for golive, name in golives.items():
+        if golive > start_time:
+            plt.axvline(golive, c='k', ls='-.', label=name)
+
+    if in_logit_diff_space:
+        default_top = pos_sent_to_logit_diff(MOOD_NAME_TO_DYNAMIC_MOOD_VALUE_MAP['only_happy'])+1.5
+        default_bottom = pos_sent_to_logit_diff(MOOD_NAME_TO_DYNAMIC_MOOD_VALUE_MAP['only_sad'])-1.5
+
+        plt.ylim(min(default_bottom, min(bottoms)), max(default_top, max(tops)))
+
+    plt.legend(fontsize=12, );
+    plt.tick_params(labelsize=12)
+    plt.tick_params(axis='x', labelrotation=60)
+
+    plt.show()

69ef66b65fd239420aee9516d46eb1fddb1ec90e
nostalgebraist <nostalgebraist@gmail.com>
2021-02-04 15:40:53 -0800

    feb sync: fixes + formatting

--- mood.py
+++ mood.py
@@ -7,63 +7,76 @@ import numpy as np, pandas as pd
 
 DEFAULT_MOOD = "unrestricted"
 
+
 def logit_diff_to_pos_sent(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def pos_sent_to_logit_diff(x, eps=1e-4):
-    return -np.log(1/min(max(x, eps), 1-eps)-1)
+    return -np.log(1 / min(max(x, eps), 1 - eps) - 1)
+
 
 def load_logit_diff_sample():
-    with open("logit_diff_sample.pkl.gz", "rb") as f:
+    with open("data/logit_diff_sample.pkl.gz", "rb") as f:
         logit_diff_sample = pickle.load(f)
     return pd.Series(logit_diff_sample)
 
-def estimate_expected_rejections(min_logit_diff: float, max_logit_diff: float, logit_diff_sample_series: pd.Series):
-    return ((logit_diff_sample_series < min_logit_diff) | (logit_diff_sample_series > max_logit_diff)).mean()
+
+def estimate_expected_rejections(
+    min_logit_diff: float, max_logit_diff: float, logit_diff_sample_series: pd.Series
+):
+    return (
+        (logit_diff_sample_series < min_logit_diff)
+        | (logit_diff_sample_series > max_logit_diff)
+    ).mean()
+
 
 def get_mood_by_name(mood_name: str):
-    bound_names = {"no_lower_bound": 0.,
-                   "no_upper_bound": 1.}
+    bound_names = {"no_lower_bound": 0.0, "no_upper_bound": 1.0}
 
     moods_original_flavor = {
         "only_sad": {
             "min_allowed_score": bound_names["no_lower_bound"],
             "max_allowed_score": 0.1,
-            "score_fn": "pos_sentiment"
-            },
+            "score_fn": "pos_sentiment",
+        },
         "only_non_happy": {
             "min_allowed_score": bound_names["no_lower_bound"],
             "max_allowed_score": 0.2,
-            "score_fn": "pos_sentiment"
-            },
+            "score_fn": "pos_sentiment",
+        },
         "meh": {
             "min_allowed_score": 0.02,
             "max_allowed_score": 0.6,
             "score_fn": "pos_sentiment",
-            },
+        },
         "only_non_sad": {
             "min_allowed_score": 0.15,
             "max_allowed_score": bound_names["no_upper_bound"],
-            "score_fn": "pos_sentiment"
-            },
+            "score_fn": "pos_sentiment",
+        },
         "only_happy": {
             "min_allowed_score": 0.4,
             "max_allowed_score": bound_names["no_upper_bound"],
-            "score_fn": "pos_sentiment"
-            },
+            "score_fn": "pos_sentiment",
+        },
         "unrestricted": {
             "min_allowed_score": bound_names["no_lower_bound"],
             "max_allowed_score": bound_names["no_upper_bound"],
-            "score_fn": "pos_sentiment"
-            }
-        }
+            "score_fn": "pos_sentiment",
+        },
+    }
 
     moods_logit_diff_version = {}
     for k, v in moods_original_flavor.items():
         moods_logit_diff_version[k] = v
 
-        moods_logit_diff_version[k]["min_allowed_score"] = pos_sent_to_logit_diff(moods_logit_diff_version[k]["min_allowed_score"])
-        moods_logit_diff_version[k]["max_allowed_score"] = pos_sent_to_logit_diff(moods_logit_diff_version[k]["max_allowed_score"])
+        moods_logit_diff_version[k]["min_allowed_score"] = pos_sent_to_logit_diff(
+            moods_logit_diff_version[k]["min_allowed_score"]
+        )
+        moods_logit_diff_version[k]["max_allowed_score"] = pos_sent_to_logit_diff(
+            moods_logit_diff_version[k]["max_allowed_score"]
+        )
         moods_logit_diff_version[k]["score_fn"] = "logit_diff"
     moods = moods_logit_diff_version
 
@@ -74,23 +87,27 @@ def get_mood_by_name(mood_name: str):
     moods = moods_
 
     if mood_name.startswith("interp_"):
-        segments = mood_name[len("interp_"):].split("__")
+        segments = mood_name[len("interp_") :].split("__")
 
         lower_mood = moods[segments[0]]
         upper_mood = moods[segments[1]]
         lower_frac = float(segments[2])
         upper_frac = float(segments[3])
 
-        interp_min_allowed_score = (lower_frac * lower_mood['min_allowed_score']) + (
-            upper_frac * upper_mood['min_allowed_score'])
+        interp_min_allowed_score = (lower_frac * lower_mood["min_allowed_score"]) + (
+            upper_frac * upper_mood["min_allowed_score"]
+        )
 
-        interp_max_allowed_score = (lower_frac * lower_mood['max_allowed_score']) + (
-            upper_frac * upper_mood['max_allowed_score'])
+        interp_max_allowed_score = (lower_frac * lower_mood["max_allowed_score"]) + (
+            upper_frac * upper_mood["max_allowed_score"]
+        )
 
-        interp_mood = {"name": mood_name,
-                       "score_fn": "logit_diff",
-                       "min_allowed_score": interp_min_allowed_score,
-                       "max_allowed_score": interp_max_allowed_score}
+        interp_mood = {
+            "name": mood_name,
+            "score_fn": "logit_diff",
+            "min_allowed_score": interp_min_allowed_score,
+            "max_allowed_score": interp_max_allowed_score,
+        }
         return interp_mood
 
     if mood_name not in moods:
@@ -98,13 +115,14 @@ def get_mood_by_name(mood_name: str):
         return DEFAULT_MOOD
     return moods[mood_name]
 
+
 def random_mood_at_pst_datetime(dt: datetime, verbose=True):
     if dt > datetime(2020, 5, 27, 9, 24, 43):
         mood_options = [
             "only_non_happy",
             "meh",
             "only_non_sad",
-            ]
+        ]
     else:
         # legacy (but post-moon)
         mood_options = [
@@ -113,10 +131,10 @@ def random_mood_at_pst_datetime(dt: datetime, verbose=True):
             "unrestricted",
             "only_non_sad",
             "only_happy",
-            ]
+        ]
 
     # for continuity with moon
-    if dt.strftime('%Y-%m-%d') == "2020-05-22":
+    if dt.strftime("%Y-%m-%d") == "2020-05-22":
         return "only_non_sad"
 
     seed = int(str(dt.year) + str(dt.month) + str(dt.day))

22e4e35dc3af07d9ca05e157e04d52eef22d3acd
nostalgebraist <nostalgebraist@gmail.com>
2021-02-04 16:57:11 -0800

    feb sync: linting, still need to deal with star imports

--- autoresponder_config.py
+++ autoresponder_config.py
@@ -44,14 +44,17 @@ DO_ALT_TIMESTAMPS = False  # True is for analytics
 eot_end_segment = EOT_FULL if EOT_WORKAROUND else "<|"
 
 if V10:
-  final_munge_before_neural = final_munge_before_neural_v10
-  final_munge_after_neural = final_munge_after_neural_v10
+    final_munge_before_neural = final_munge_before_neural_v10
+    final_munge_after_neural = final_munge_after_neural_v10
 elif V8:
-  final_munge_before_neural = final_munge_before_neural_v8
-  final_munge_after_neural = final_munge_after_neural_v8
+    final_munge_before_neural = final_munge_before_neural_v8
+    final_munge_after_neural = final_munge_after_neural_v8
 else:
-  final_munge_before_neural = final_munge_before_neural_v7
-  final_munge_after_neural = lambda text, *args, **kwargs: text
+    final_munge_before_neural = final_munge_before_neural_v7
+
+    def final_munge_after_neural(text, *args, **kwargs):
+        return text
+
 
 if V10:
     model_name = "autoresponder_v10"
@@ -64,8 +67,12 @@ if V10:
     SELECTOR_EOT_PREPEND = True
 
     gs_command_get_model = f"gsutil -m cp gs://{BUCKET_NAME}/checkpoint_gs_sync/autoresponder_v10_nost_tuning_f/model-135.hdf5 /models/autoresponder_v10/"
-    gs_command_get_selector = f"gsutil -m cp -R gs://{BUCKET_NAME}/ar_model_v10/v10_selector/* /selector/"
-    gs_command_get_sentiment = f"gsutil -m cp -R gs://{BUCKET_NAME}/ar_model_v10/v10_sentiment/* /sentiment/"
+    gs_command_get_selector = (
+        f"gsutil -m cp -R gs://{BUCKET_NAME}/ar_model_v10/v10_selector/* /selector/"
+    )
+    gs_command_get_sentiment = (
+        f"gsutil -m cp -R gs://{BUCKET_NAME}/ar_model_v10/v10_sentiment/* /sentiment/"
+    )
 elif V9_1R4:
     model_name = "autoresponder_v9_v1_1558M_nost_tuning4"
 
@@ -115,13 +122,13 @@ elif V8:
     model_name = "autoresponder_v8_opt"
 
     if V8_2:
-      dataset = "data/data_v8_2_recat.npz"
-      ckpt_select = "autoresponder_v8_2_selector/v7/.hdf5"
-      ckpt_sentiment = "autoresponder_v8_2_sentiment/v1/.hdf5"
+        dataset = "data/data_v8_2_recat.npz"
+        ckpt_select = "autoresponder_v8_2_selector/v7/.hdf5"
+        ckpt_sentiment = "autoresponder_v8_2_sentiment/v1/.hdf5"
     else:
-      dataset = "data/data_v8_1_extended.npz"
-      ckpt_select = "autoresponder_v8_selector/v9/.hdf5"
-      ckpt_sentiment = "autoresponder_v8_sentiment/v1/.hdf5"
+        dataset = "data/data_v8_1_extended.npz"
+        ckpt_select = "autoresponder_v8_selector/v9/.hdf5"
+        ckpt_sentiment = "autoresponder_v8_sentiment/v1/.hdf5"
     TRUNCATE_AT_RIGHT = True
     SELECTOR_EOT_PREPEND = True
 elif FORUMLIKE_V2:
@@ -151,9 +158,9 @@ else:
     TRUNCATE_AT_RIGHT = False
 
 with open(ckpt_select.rpartition("/")[0] + "/metadata.json", "r") as f:
-  select_metadata = json.load(f)
+    select_metadata = json.load(f)
 
-select_scope = select_metadata['select_scope']
+select_scope = select_metadata["select_scope"]
 
 if V9:
     layer_nums = [7, 23]
@@ -169,7 +176,7 @@ if V9:
     add_position_emb_later_layers = False
     add_prompt_cont_embs = False
     norm_final_output = False
-    n_head_select = select_metadata['n_head']
+    n_head_select = select_metadata["n_head"]
     length_select = 825
     SELECT_VIA_GENERATOR_LONGLENGTH = True
     MULTI_LR_CALIB = True
@@ -187,7 +194,7 @@ elif V8:
     add_position_emb_later_layers = False
     add_prompt_cont_embs = False
     norm_final_output = False
-    n_head_select = select_metadata['n_head']
+    n_head_select = select_metadata["n_head"]
     length_select = 825
     SELECT_VIA_GENERATOR_LONGLENGTH = True
     MULTI_LR_CALIB = True
@@ -214,72 +221,74 @@ SENTIMENT_VIA_GENERATOR = True
 SENTIMENT_VIA_GENERATOR_LONGLENGTH = True
 
 with open(ckpt_sentiment.rpartition("/")[0] + "/metadata.json", "r") as f:
-  sentiment_select_metadata = json.load(f)
+    sentiment_select_metadata = json.load(f)
 
-sentiment_select_scope = sentiment_select_metadata['select_scope']
+sentiment_select_scope = sentiment_select_metadata["select_scope"]
 
 if V9_1:
-  layer_nums_sentiment = [7, 23]
-  use_mlp_sentiment = True
-  use_length_channel_sentiment = False
-  use_length_channel_v2_sentiment = False
-  length_sentiment = 204
-  norm_final_output_sentiment = False
-  n_head_sentiment = sentiment_select_metadata['n_head']
+    layer_nums_sentiment = [7, 23]
+    use_mlp_sentiment = True
+    use_length_channel_sentiment = False
+    use_length_channel_v2_sentiment = False
+    length_sentiment = 204
+    norm_final_output_sentiment = False
+    n_head_sentiment = sentiment_select_metadata["n_head"]
 elif V9:
-  layer_nums_sentiment = [7, 23]
-  use_mlp_sentiment = True
-  use_length_channel_sentiment = False
-  use_length_channel_v2_sentiment = False
-  length_sentiment = 204
-  norm_final_output_sentiment = False
-  n_head_sentiment = sentiment_select_metadata['n_head']
+    layer_nums_sentiment = [7, 23]
+    use_mlp_sentiment = True
+    use_length_channel_sentiment = False
+    use_length_channel_v2_sentiment = False
+    length_sentiment = 204
+    norm_final_output_sentiment = False
+    n_head_sentiment = sentiment_select_metadata["n_head"]
 elif V8:
-  layer_nums_sentiment = [8-1, 24-1]
-  use_mlp_sentiment = True
-  use_length_channel_sentiment = False
-  use_length_channel_v2_sentiment = False
-  length_sentiment = 204
-  norm_final_output_sentiment = False
-  n_head_sentiment = sentiment_select_metadata.get('n_head', 25)
+    layer_nums_sentiment = [8 - 1, 24 - 1]
+    use_mlp_sentiment = True
+    use_length_channel_sentiment = False
+    use_length_channel_v2_sentiment = False
+    length_sentiment = 204
+    norm_final_output_sentiment = False
+    n_head_sentiment = sentiment_select_metadata.get("n_head", 25)
 else:
-  layer_nums_sentiment = [8-1, 24-1]
-  use_mlp_sentiment = False
-  use_length_channel_sentiment = False
-  use_length_channel_v2_sentiment = False
-  length_sentiment = 204
-  norm_final_output_sentiment = True
-  n_head_sentiment = 25
+    layer_nums_sentiment = [8 - 1, 24 - 1]
+    use_mlp_sentiment = False
+    use_length_channel_sentiment = False
+    use_length_channel_v2_sentiment = False
+    length_sentiment = 204
+    norm_final_output_sentiment = True
+    n_head_sentiment = 25
 
 # hack
 if n_head_sentiment is None:
     n_head_sentiment = 25
 
+
 def _gpu_type():
-  try:
-      gpu_info = subprocess.check_output("nvidia-smi").decode('utf-8')
-  except:
-    return "small"
-  if "P100" in gpu_info:
-    return "big"
-  else:
-    return "small"
+    try:
+        gpu_info = subprocess.check_output("nvidia-smi").decode("utf-8")
+    except:
+        return "small"
+    if "P100" in gpu_info:
+        return "big"
+    else:
+        return "small"
+
 
 if BATCHONE:
-  batch_size = 1
+    batch_size = 1
 
-  if _gpu_type() == "big":
-    max_ctx_fits_on_gpu=1020
-  else:
-    max_ctx_fits_on_gpu=1020
+    if _gpu_type() == "big":
+        max_ctx_fits_on_gpu = 1020
+    else:
+        max_ctx_fits_on_gpu = 1020
 
 else:
-  batch_size = 2
+    batch_size = 2
 
-  if _gpu_type() == "big":
-    max_ctx_fits_on_gpu=940  #825
-  else:
-    max_ctx_fits_on_gpu=840  #1020  #800
+    if _gpu_type() == "big":
+        max_ctx_fits_on_gpu = 940  # 825
+    else:
+        max_ctx_fits_on_gpu = 840  # 1020  #800
 
 
 # sets max context size, for long prompts we want to cut off to allow bot to write at least this many tokens
@@ -289,14 +298,14 @@ if EVEN_BETTER_LENGTH:
     better_length = False
     length = required_continuation_room
     # MAX_CONTINUE_TOKENS=2210 if _gpu_type() == "big" else 1600
-    MAX_CONTINUE_TOKENS=1600
+    MAX_CONTINUE_TOKENS = 1600
 
-    MAX_CONTINUE_STEPS=100  # disable via huge
+    MAX_CONTINUE_STEPS = 100  # disable via huge
 else:
     better_length = True
-    length=max_ctx_fits_on_gpu
-    MAX_CONTINUE_TOKENS=12*1024  # disable via huge
-    MAX_CONTINUE_STEPS=12
+    length = max_ctx_fits_on_gpu
+    MAX_CONTINUE_TOKENS = 12 * 1024  # disable via huge
+    MAX_CONTINUE_STEPS = 12
 
 ### Sampling
 
@@ -336,8 +345,8 @@ if RANDOM_SAMPLING_PARAMS_ON_STARTUP:
             (0.95, 0.9, None, None),
             (0.9, 0.9, None, None),
             (0.9, 0.97, None, None),
-            (1, 0, (1-0.9), 0.03),  # chop_lowest 1-x = top_p x
-            (0.9, 0, (1-0.9), 0.05),  # chop_lowest 1-x = top_p x
+            (1, 0, (1 - 0.9), 0.03),  # chop_lowest 1-x = top_p x
+            (0.9, 0, (1 - 0.9), 0.05),  # chop_lowest 1-x = top_p x
         ]
     ]
 
@@ -356,12 +365,12 @@ if RANDOM_SAMPLING_PARAMS_ON_STARTUP:
     msg = f"\nSAMPLING: using bundle {bundle_ix}/{len(choose_from_bundles)}\n\t{chosen_bundle}"
     print(msg)
 
-    MIRO = chosen_bundle['MIRO']
-    MIRO_LR = chosen_bundle['MIRO_LR']
-    temperature = chosen_bundle['T']
-    top_p = chosen_bundle['p']
-    chop_lowest = chosen_bundle['chop_lowest']
-    chop_highest = chosen_bundle['chop_highest']
+    MIRO = chosen_bundle["MIRO"]
+    MIRO_LR = chosen_bundle["MIRO_LR"]
+    temperature = chosen_bundle["T"]
+    top_p = chosen_bundle["p"]
+    chop_lowest = chosen_bundle["chop_lowest"]
+    chop_highest = chosen_bundle["chop_highest"]
 
     top_k = 0
     middle_p = 0
@@ -369,12 +378,12 @@ if RANDOM_SAMPLING_PARAMS_ON_STARTUP:
     MIRO_ONLY_ON_CONTINUE = MIRO
 else:
     MIRO = True
-MIRO_ONLY_ON_CONTINUE = True
+    MIRO_ONLY_ON_CONTINUE = True
 
-    temperature=1
-    top_k=0
-    top_p=0
-    middle_p=0
+    temperature = 1
+    top_k = 0
+    top_p = 0
+    middle_p = 0
 
     # unused
     EXPERIMENTAL_TOP_P = True
@@ -393,68 +402,94 @@ MIRO_ONLY_ON_CONTINUE = True
     MIRO_LR = 0.05
 
 if V8_2:
-  MIRO_TARGET_LOW = 2.0
-  MIRO_TARGET_HIGH = 2.8
-  MIRO_TARGET_ALL = [
-    1.8,
-    1.9, 1.9,
-    2.0, 2.0,
-    2.1, 2.1, 2.1,
-    2.2, 2.2, 2.2,
-    2.3, 2.3, 2.3,
-    2.4, 2.4,
-    2.5,
-    2.6,
-    2.7,
-    2.8,
-    2.9,
-    3.0,
+    MIRO_TARGET_LOW = 2.0
+    MIRO_TARGET_HIGH = 2.8
+    MIRO_TARGET_ALL = [
+        1.8,
+        1.9,
+        1.9,
+        2.0,
+        2.0,
+        2.1,
+        2.1,
+        2.1,
+        2.2,
+        2.2,
+        2.2,
+        2.3,
+        2.3,
+        2.3,
+        2.4,
+        2.4,
+        2.5,
+        2.6,
+        2.7,
+        2.8,
+        2.9,
+        3.0,
     ]
 
 else:
-  MIRO_TARGET_LOW = 2.0
-  MIRO_TARGET_HIGH = 2.8
-  MIRO_TARGET_ALL = [
-    2.0, 2.0,
-    2.1, 2.1, 2.1, 2.1,
-    2.2, 2.2, 2.2,
-    2.3, 2.3,
-    2.4, 2.4,
-    2.5,
-    2.6,
-    2.7,
-    2.8, 2.8,
+    MIRO_TARGET_LOW = 2.0
+    MIRO_TARGET_HIGH = 2.8
+    MIRO_TARGET_ALL = [
+        2.0,
+        2.0,
+        2.1,
+        2.1,
+        2.1,
+        2.1,
+        2.2,
+        2.2,
+        2.2,
+        2.3,
+        2.3,
+        2.4,
+        2.4,
+        2.5,
+        2.6,
+        2.7,
+        2.8,
+        2.8,
     ]
 
 
 if MIRO_ONLY_ON_CONTINUE:
-  pre_continue_mirostat=False
-  pre_continue_length=80 # 50
-  pre_continue_temperature=0.9 #1
-  pre_continue_top_k=0
-  pre_continue_top_p=0.9 #0.925
-  pre_continue_middle_p=0
-  pre_continue_chop_lowest=None
-  pre_continue_chop_highest=None
+    pre_continue_mirostat = False
+    pre_continue_length = 80  # 50
+    pre_continue_temperature = 0.9  # 1
+    pre_continue_top_k = 0
+    pre_continue_top_p = 0.9  # 0.925
+    pre_continue_middle_p = 0
+    pre_continue_chop_lowest = None
+    pre_continue_chop_highest = None
 else:
-  pre_continue_mirostat=MIRO
-  pre_continue_length=length
-  pre_continue_temperature=temperature
-  pre_continue_top_k=top_k
-  pre_continue_top_p=top_p
-  pre_continue_middle_p=middle_p
-  pre_continue_chop_lowest=chop_lowest
-  pre_continue_chop_highest=chop_highest
+    pre_continue_mirostat = MIRO
+    pre_continue_length = length
+    pre_continue_temperature = temperature
+    pre_continue_top_k = top_k
+    pre_continue_top_p = top_p
+    pre_continue_middle_p = middle_p
+    pre_continue_chop_lowest = chop_lowest
+    pre_continue_chop_highest = chop_highest
 
 print((MIRO, length, temperature, top_p, middle_p, chop_lowest, chop_highest))
-print((pre_continue_mirostat, pre_continue_length, pre_continue_temperature,
-       pre_continue_top_p, pre_continue_middle_p,
-       pre_continue_chop_lowest, pre_continue_chop_highest))
+print(
+    (
+        pre_continue_mirostat,
+        pre_continue_length,
+        pre_continue_temperature,
+        pre_continue_top_p,
+        pre_continue_middle_p,
+        pre_continue_chop_lowest,
+        pre_continue_chop_highest,
+    )
+)
 
 if SELECT_VIA_GENERATOR_LONGLENGTH:
-  length_select = max_ctx_fits_on_gpu
+    length_select = max_ctx_fits_on_gpu
 
 if SENTIMENT_VIA_GENERATOR_LONGLENGTH:
-  length_sentiment = max_ctx_fits_on_gpu
+    length_sentiment = max_ctx_fits_on_gpu
 
 os.chdir(startdir)

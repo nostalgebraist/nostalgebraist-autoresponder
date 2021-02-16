d5b17b9fbc65101e091145db0208744183720f6e
nostalgebraist <nostalgebraist@gmail.com>
2021-02-16 13:04:43 -0800

    remove unused

--- bridge_service.py
+++ bridge_service.py
@@ -2,56 +2,16 @@
 Coordinating switchboard that handles communication between the generator, selector, and
 tumblr API compontents.
 """
-import numpy as np
-import uuid
-from datetime import datetime
-from itertools import chain
-
 from flask import Flask, request, jsonify
 
 from bot_config import BotSpecificConstants
-from mood import get_mood_by_name, load_logit_diff_sample, estimate_expected_rejections
-from bridge_shared import bridge_service_unique_id
-
-from autoresponder_static import DEFAULT_CSC
-from autoresponder_static_v8 import timestamp_to_v10_format
 
 bot_specific_constants = BotSpecificConstants.load()
 bridge_service_port = bot_specific_constants.bridge_service_port
 bridge_service_url = bot_specific_constants.bridge_service_url
 
-TRADE_QUALITY_FOR_SPEED = False
-
-logit_diff_sample_series = load_logit_diff_sample()
-EXPECTED_REJECTION_MULT = 0.5 if (not TRADE_QUALITY_FOR_SPEED) else 0.4
-
-# note to self: trying to be more random about textposts / use retention_stack less
-# to give the selector more training signal
-#
-# interventions:
-#   - fewer candidates
-#   - higher eps in eps_greedy
-#   - higher retention_stack proba cutoff (to the point that the stack is usu. small)
-TEXTPOST_N_CANDIDATES_TARGET = 15 if (not TRADE_QUALITY_FOR_SPEED) else 12
-# TEXTPOST_N_CANDIDATES_TARGET = 20 if (not TRADE_QUALITY_FOR_SPEED) else 18
-
-Q_CHAR = "会"
-A_CHAR = "域"
-T_CHAR = "职"
-
-UNAME_CHAR = "友"
-ORIG_POST_CHAR = "翰"
-
-AB_TEST_SELECTOR = True
-AB_TEST_A_SEQUENCE = "\uFFF9"
-AB_TEST_B_SEQUENCE = "\uFFF9\uFFFA\uFFFB"
-
 PROMPT_STACK = {}
 RESULT_STACK = {}
-
-GENERATION_RESULT_STACK = {}
-
-GENERATIONS_PER_REQUEST = 1
 
 ### FLASK
 app = Flask(__name__)

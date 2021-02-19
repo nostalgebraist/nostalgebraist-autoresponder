e91ad261a56f729cd74ef1bea265a4ee9131569b
nostalgebraist <nostalgebraist@gmail.com>
2021-02-19 08:40:17 -0800

    fix edge case part 2

--- reply_munging.py
+++ reply_munging.py
@@ -32,7 +32,7 @@ def post_body_find_reply_data(post_body: str):
 
     url = seg2.partition(">")[0]
     urlseg = url.partition("/post/")[2]
-    pid = urlseg.rstrip("/\"")
+    pid = urlseg.rstrip("\"").partition('/')[0]
 
     replier_seg = seg1
     if replier_seg.endswith(">"):

28b71ae52426943d15987666159662fcbee9da19
nostalgebraist <nostalgebraist@gmail.com>
2021-02-20 17:35:07 -0800

    lower pre_continue_length and raise required_continuation_room

--- autoresponder_config.py
+++ autoresponder_config.py
@@ -106,7 +106,7 @@ else:
 
 
 # sets max context size, for long prompts we want to cut off to allow bot to write at least this many tokens
-required_continuation_room = 100
+required_continuation_room = 200
 
 if EVEN_BETTER_LENGTH:
     better_length = False
@@ -270,7 +270,7 @@ else:
 
 if MIRO_ONLY_ON_CONTINUE:
     pre_continue_mirostat = False
-    pre_continue_length = 80  # 50
+    pre_continue_length = 30  # 50
     pre_continue_temperature = 0.9  # 1
     pre_continue_top_k = 0
     pre_continue_top_p = 0.9  # 0.925

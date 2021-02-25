646972177ca08c95a7aa391aacae5802e3e4e157
nostalgebraist <nostalgebraist@gmail.com>
2021-02-25 10:47:18 -0800

    bump selector version

--- autoresponder_config.py
+++ autoresponder_config.py
@@ -50,7 +50,7 @@ model_name = "autoresponder_v10"
 model_path = os.path.join("models", model_name, "model-135.hdf5")
 
 dataset = "data/v10/ALL_data_v10_nost_tuning.npz"
-ckpt_select = "selector/v10/v13/.hdf5"
+ckpt_select = "selector/v10/v14/.hdf5"
 ckpt_sentiment = "sentiment/v10/v2/.hdf5"
 
 TRUNCATE_AT_RIGHT = False

f5b6e4cd9ba339233788361c7035b9e4ecd3b4a7
nostalgebraist <nostalgebraist@gmail.com>
2021-02-26 21:40:43 -0800

    determiner center update

--- mood_dynamic.py
+++ mood_dynamic.py
@@ -58,6 +58,7 @@ DETERMINER_CENTER_UPDATES = {
     pd.Timestamp("2021-02-15 17:25:00"): 0,
     pd.Timestamp("2021-02-16 17:45:00"): 0.5,
     pd.Timestamp("2021-02-17 12:45:00"): 0,
+    pd.Timestamp("2021-02-26 17:30:00"): 0.5,
 }
 DETERMINER_MULTIPLIER_UPDATES = {
     pd.Timestamp("2020-08-25 17:00:00"): 0.1 / RESPONSE_SCALE_BASE,

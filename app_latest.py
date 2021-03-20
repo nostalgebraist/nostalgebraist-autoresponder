1aa07b716fd05eeaeb7dfa244042e62605967d9f
nostalgebraist <nostalgebraist@gmail.com>
2021-03-19 19:19:17 -0700

    fix

--- app_latest.py
+++ app_latest.py
@@ -2,7 +2,7 @@ import json
 import sys
 sys.path.append("gpt-2/")
 sys.path.append("gpt-2/src/")
-from ml_layer import handle_request
+from experimental.ml_layer import handle_request
 
 
 def lambda_handler(data, lambda_context):

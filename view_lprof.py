410ff731be6881a1fdd96168c34c00eab2cff00d
nostalgebraist <nostalgebraist@gmail.com>
2021-04-06 10:41:37 -0700

    readability

--- view_lprof.py
+++ view_lprof.py
@@ -6,7 +6,7 @@ def main(in_file="tumbl.py.lprof", out_file="lprof.txt"):
     lstats = line_profiler.load_stats(in_file)
 
     with open(out_file, 'w', encoding='utf-8') as f:
-        line_profiler.show_text(lstats.timings, lstats.unit, output_unit=1e-2, stripzeros=True, stream=f)
+        line_profiler.show_text(lstats.timings, lstats.unit, output_unit=1, stripzeros=True, stream=f)
 
 
 if __name__ == "__main__":

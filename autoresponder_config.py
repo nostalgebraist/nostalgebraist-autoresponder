04ef76dd067fe506a871d8a27d15ae14e3e4c220
nostalgebraist <nostalgebraist@gmail.com>
2021-04-14 16:37:52 -0700

    bump autoreviewer version and adjust thresholds

--- autoresponder_config.py
+++ autoresponder_config.py
@@ -18,8 +18,8 @@ V10_1 = True  # !!
 
 USE_AUTOREVIEWER = True
 AUTOREVIEWER_CUTOFFS = {
-    "accept_below": 0.12,  # v10_1/v7: predict true accept rate: ~30%, false accept rate ~5%
-    "reject_above": 0.66,  # v10_1/v6: predict true reject rate: ~48%, false reject rate ~4.5%
+    "accept_below": 0.13,  # v10_1/v8: predict true accept rate: ~43%, false accept rate ~4%
+    "reject_above": 0.55,  # v10_1/v8: predict true reject rate: ~46%, false reject rate ~4.5%
 }
 
 bot_specific_constants = BotSpecificConstants.load()
@@ -67,7 +67,7 @@ else:
 if V10_1:
     ckpt_select = "selector/v10_1/v6/.hdf5"
     ckpt_sentiment = "sentiment/v10_1/v1/.hdf5"
-    ckpt_autoreviewer = "draft_autoreviewer/v10_1/v7/.hdf5"
+    ckpt_autoreviewer = "draft_autoreviewer/v10_1/v8/.hdf5"
 else:
     ckpt_select = "selector/v10/v17/.hdf5"
     ckpt_sentiment = "sentiment/v10/v2/.hdf5"

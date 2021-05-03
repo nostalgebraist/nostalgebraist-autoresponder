dc0ff91bda796693c47e5252dfc0197bcc6b090b
nostalgebraist <nostalgebraist@gmail.com>
2021-05-03 09:30:32 -0700

    fix

--- selector_model/selector_estimator_neo.py
+++ selector_model/selector_estimator_neo.py
@@ -232,13 +232,13 @@ class NostARHeadEstimator(BaseEstimator, ClassifierMixin):
         self.sched_decay_ = get_nost_ar_head_scheduler(
             self.opt_decay_,
             self.opt_params,
-            len(X) // self.opt_params.batch_size,
+            self.opt_params.epochs * len(X) // self.opt_params.batch_size,
         )
 
         self.sched_no_decay_ = get_nost_ar_head_scheduler(
             self.opt_no_decay_,
             self.opt_params,
-            len(X) // self.opt_params.batch_size,
+            self.opt_params.epochs * len(X) // self.opt_params.batch_size,
         )
 
         self.scaler_ = torch.cuda.amp.GradScaler(enabled=self.use_amp_training)

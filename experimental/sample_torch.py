538ceb64904c87b09e040b95a40fb0da2a1fd052
nostalgebraist <nostalgebraist@gmail.com>
2021-05-05 16:08:22 -0700

    breakuns fixes

--- experimental/sample_torch.py
+++ experimental/sample_torch.py
@@ -6,16 +6,50 @@ from transformers import LogitsProcessor
 class BreakrunsLogitsProcessor(LogitsProcessor):
     def __init__(self,
                  base_temperature: float,
-                 tau: float
+                 tau: float,
+                 debug=True,
+                 tokenizer=None
                  ):
         self.breakruns_counter = None
         self.last_logits = None
         self.base_temperature = base_temperature
         self.tau = tau
+        self.debug = debug
+        self.tokenizer = tokenizer
+
+    def _dprint(self, *args, **kwargs):
+        if self.debug:
+            print(*args, **kwargs)
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        seq_length = input_ids.shape[1]
+        if seq_length < 1:
+            self._dprint("BREAKRUNS: empty sequence, no op")
+            return scores
+
         if self.breakruns_counter is None:
-            self.breakruns_counter = torch.as_tensor(1, device=input_ids.device)
-            
+            self._dprint("BREAKRUNS: init counter")
+            self.breakruns_counter = torch.zeros((), device=input_ids.device)
+
         if self.last_logits is None:
+            self._dprint("BREAKRUNS: init logits, no op")
             self.last_logits = scores
+
+            return scores
+
+        # check if last was top
+        was_top = (input_ids[:, -1] == self.last_logits.argmax(dim=1)).to(torch.long)
+
+        self.breakruns_counter = was_top * (self.breakruns_counter + 1)
+
+        if self.debug:
+            sampled_str = repr(self.tokenizer.decode(input_ids[:, -1].item()))
+            actual_top_str = repr(self.tokenizer.decode([self.last_logits.argmax(dim=1).item()]))
+            print(f"was_top?: {was_top} | sampled {sampled_str} actual_top {actual_top_str} | self.breakruns_counter: {self.breakruns_counter}")
+
+        eff_temperature = self.base_temperature + (self.breakruns_counter * self.tau)
+        self._dprint(f"eff_temperature: {eff_temperature}")
+
+        self.last_logits = scores
+
+        return scores / eff_temperature

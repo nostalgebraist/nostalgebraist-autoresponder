2857f842332d8545f79d1c15ca4b8788bdaeafae
nostalgebraist <nostalgebraist@gmail.com>
2021-05-05 12:21:24 -0700

    fix

--- experimental/generator_model_torch.py
+++ experimental/generator_model_torch.py
@@ -102,7 +102,7 @@ class GeneratorModelTorch:
             dones = []
             for i, o in enumerate(out):
                 # record the tokens
-                extras = o.cpu().numpy()
+                extras = o[1:].cpu().numpy()
                 nonpads = [t for t in extras if t != self.tokenizer.pad_token_id]
                 continuations_tokens[i].extend(nonpads)
 

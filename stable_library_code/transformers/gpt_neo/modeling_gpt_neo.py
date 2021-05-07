6196a7b53d997a67e456911f3c329cec22a49cf7
nostalgebraist <nostalgebraist@gmail.com>
2021-05-07 08:58:18 -0700

    remove unused pad-to-256 helpers

--- stable_library_code/transformers/gpt_neo/modeling_gpt_neo.py
+++ stable_library_code/transformers/gpt_neo/modeling_gpt_neo.py
@@ -19,7 +19,6 @@ import os
 from typing import Tuple
 
 import torch
-import torch.nn.functional as F
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss
@@ -723,13 +722,7 @@ class GPTNeoForCausalLM(GPTNeoPreTrainedModel):
     def set_output_embeddings(self, new_embeddings):
         self.lm_head = new_embeddings
 
-    def prepare_inputs_for_generation(self, input_ids, past=None, user_past=None, **kwargs):
-        # print(f"kwargs: {kwargs}")
-        if past is None and user_past is None:
-            print(f"No past or user_past.  This should only happen when recomputing past")
-        if past is None and user_past is not None:
-            past = user_past
-
+    def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
         token_type_ids = kwargs.get("token_type_ids", None)
         # only last token for inputs_ids if past is defined in kwargs
         if past:

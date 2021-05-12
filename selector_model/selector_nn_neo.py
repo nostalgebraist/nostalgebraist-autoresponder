1629ad0362c25c33c6f58a5b476afb72c7245a77
nostalgebraist <nostalgebraist@gmail.com>
2021-05-12 11:41:44 -0700

    fix

--- selector_model/selector_nn_neo.py
+++ selector_model/selector_nn_neo.py
@@ -159,10 +159,14 @@ class NostARHeadAttention(nn.Module, GPTNeoAttentionMixin):
 
     def classic_init(self, gain: float = 1.):
         with torch.no_grad():
-            qkv_weight = torch.empty(self.embed_dim, 3 * self.embed_dim, requires_grad=False)
+            qkv_weight = torch.empty(self.embed_dim, 2 * self.embed_dim + self.proj_dim, requires_grad=False)
             nn.init.orthogonal_(qkv_weight, gain)
 
-            q_weight, k_weight, v_weight = torch.split(qkv_weight, self.embed_dim, dim=-1)
+            q_weight, k_weight, v_weight = torch.split(
+                qkv_weight,
+                [self.embed_dim, self.embed_dim, self.proj_dim],
+                dim=-1
+            )
 
             self.q_proj.weight.copy_(q_weight)
             self.k_proj.weight.copy_(k_weight)

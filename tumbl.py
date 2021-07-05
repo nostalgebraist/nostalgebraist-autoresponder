c493cc859580fe3570027b1695f11199d841f93d
nostalgebraist <nostalgebraist@gmail.com>
2021-07-05 09:32:47 -0700

    archive_to_corpus debug

--- tumbl.py
+++ tumbl.py
@@ -2848,10 +2848,12 @@ def mainloop(loop_persistent_data: LoopPersistentData, response_cache: ResponseC
                     image_analysis_cache.save()
         return loop_persistent_data, response_cache
 
-    ### do asks check
-    loop_persistent_data, response_cache = _mainloop_asks_block(
-        loop_persistent_data, response_cache
-    )
+    ### DEBUG
+    n_posts_to_check = 1
+    # ### do asks check
+    # loop_persistent_data, response_cache = _mainloop_asks_block(
+    #     loop_persistent_data, response_cache
+    # )
 
     ### do reblog/reply check
     if n_posts_to_check > 0:

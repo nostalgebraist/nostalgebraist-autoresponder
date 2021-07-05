060b9ca3ee5285e150617a05f280656caccba6a6
nostalgebraist <nostalgebraist@gmail.com>
2021-07-05 09:13:33 -0700

    legacy simulating in get_me_clients

--- util/clients.py
+++ util/clients.py
@@ -7,7 +7,11 @@ def get_me_clients(npf=True):
     clients = bot_specific_constants.private_clients + bot_specific_constants.dashboard_clients
 
     if npf:
+        import munging_shared
+
         for cl in clients:
             cl.npf_consumption_on()
+
+        clients = [munging_shared.LegacySimulatingClient.from_rate_limit_client(cl) for cl in clients]
 
     return clients

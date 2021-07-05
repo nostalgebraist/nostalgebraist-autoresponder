8ccf31464ce6ec85d357521de7493322a2c8eec5
nostalgebraist <nostalgebraist@gmail.com>
2021-07-05 07:31:59 -0700

    reduce log spam

--- tumbl.py
+++ tumbl.py
@@ -1426,10 +1426,10 @@ def is_statically_reblog_worthy_on_dash(
         reblog_worthy = False
 
     if not reblog_worthy and (n_img == 0):
-        # pass
+        pass
         # the following logs extra posts:
-        print(f"processing {post_identifier} | ", end="")
-        write_text_for_side_judgment(post_payload, dump_to_file=False)
+        # print(f"processing {post_identifier} | ", end="")
+        # write_text_for_side_judgment(post_payload, dump_to_file=True)
 
     return reblog_worthy
 

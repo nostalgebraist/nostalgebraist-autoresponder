3132a7c58314e93292b641cb36ff5cd44fc02a9e
nostalgebraist <nostalgebraist@gmail.com>
2021-07-06 15:02:29 -0700

    fix

--- src/experimental/nwo.py
+++ src/experimental/nwo.py
@@ -31,7 +31,7 @@ def post_payload_to_formatted_text(post_payload: dict, ml_prompt_format: bool = 
     )
 
 
-def npf_thread_to_formatted_text(thread: TumblrThread,, ml_prompt_format: bool = False, control_seg_config: dict = DEFAULT_CSC):
+def npf_thread_to_formatted_text(thread: TumblrThread, ml_prompt_format: bool = False, control_seg_config: dict = DEFAULT_CSC):
     is_ask = [False for _ in thread.posts]
 
     has_ask, posts_with_ask = expand_asks(thread)

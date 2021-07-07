0748b269379f363a9bf268e7216dbb4d8a724c63
nostalgebraist <nostalgebraist@gmail.com>
2021-07-07 16:30:52 -0700

    cleanup

--- src/munging/reblogs_v5.py
+++ src/munging/reblogs_v5.py
@@ -1,20 +1,3 @@
-"""mysterious hacky html parsing that somehow (mostly?) successfully handles tumblr's weird pre-NPF markup format"""
-from munging.autoresponder_static import Q_CHAR, V10_ASK_CHAR
-
-from multimodal import image_analysis_singleton
-
-image_analysis_cache = image_analysis_singleton.IMAGE_ANALYSIS_CACHE
-
-START_DUMMY = "⭒"
-
-V10_CHARS_TO_LEGACY_CHARS = {V10_ASK_CHAR: Q_CHAR}
-
-ALWAYS_USE_A_CHAR_OPERATIONAL = True
-TRY_LINKS_FOR_IMAGES = False
-INCLUDE_HREF_FOR_A = True
-
-EOT = "<|endoftext|>"
-
 RECURSE_INTO = {
     "p",
     "blockquote",

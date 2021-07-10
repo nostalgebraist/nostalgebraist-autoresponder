2fae93ea0825f8ee5f5873e77e7a0ae0125791e9
nostalgebraist <nostalgebraist@gmail.com>
2021-07-09 17:36:30 -0700

    undo temp change

--- src/tumbl.py
+++ src/tumbl.py
@@ -853,7 +853,7 @@ class LoopPersistentData:
         timestamps={},
         reblog_keys={},
         n_posts_to_check_base=250,
-        n_posts_to_check_dash=340, # 690,
+        n_posts_to_check_dash=690,
         n_notifications_to_check=1000,
         offset_=0,
         requests_per_check_history=[],

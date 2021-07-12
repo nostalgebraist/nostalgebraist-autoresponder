cd8239526efcff3f928f6b06ea37ba0327c73655
nostalgebraist <nostalgebraist@gmail.com>
2021-07-12 16:30:40 -0700

    hmm nope

--- src/experimental/prob_delta.py
+++ src/experimental/prob_delta.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 from api_tumblr.tumblr_parsing import TumblrThread
 from tumblr_to_text.nwo import npf_thread_to_formatted_text, expand_asks
+from tumblr_to_text.nwo_munging import add_empty_reblog
 
 from api_ml.ml_connector import prob_delta_from_gpt
 
 
 def construct_prob_delta_prompts(thread: TumblrThread):
+    thread = add_empty_reblog(thread, 'DUMMYUSER', datetime.now())
+
     prompt = npf_thread_to_formatted_text(thread, prob_delta_format=True)
 
     prompt_ref = prompt.splitlines()[-1]

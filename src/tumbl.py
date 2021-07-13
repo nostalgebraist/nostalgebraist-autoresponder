81f3e91b0c93cce25b7ed9ac98e21aeb5dd2bb82
nostalgebraist <nostalgebraist@gmail.com>
2021-07-13 11:20:23 -0700

    prioritization: fix type issue

--- src/tumbl.py
+++ src/tumbl.py
@@ -805,10 +805,11 @@ def prioritize_reblogs_replies(
     reply_set,
     response_cache,
     word_cost=-1 / 10.,
-    thread_length_cost=1.,
+    thread_length_cost=1,
     short_under_n_words=5,
-    short_cost=4.,
-    empty_cost=10.
+    short_cost=4,
+    empty_cost=10,
+    verbose=False
 ):
     costs = {}
 
@@ -839,10 +840,16 @@ def prioritize_reblogs_replies(
         if word_count < 50:
             print(f"{ident}: word_count {word_count} with words {repr(user_text.split())}")
 
-        cost = (thread_length_cost * thread_length)
-        + (word_cost * word_count)
-        + (short_cost * (word_count < short_under_n_words))
-        + (empty_cost * (word_count == 0))
+        cost = 0
+        for item in [
+            (thread_length_cost, thread_length, "thread_length"),
+            (word_cost, word_count, "word_count"),
+            (short_cost, (word_count < short_under_n_words), "word_count < short_under_n_words"),
+            (empty_cost, (word_count == 0), "word_count == 0")
+        ]:
+            cost += item[0] * item[1]
+            if verbose:
+                print(f"cost now {cost:.1f} | added {item[0] * item[1]:.1f} with {item[2]}={item[1]}")
 
         costs[ident] = cost
     return costs, response_cache
@@ -2090,7 +2097,7 @@ def do_reblog_reply_handling(
                                                        reply_set=replies_to_handle,
                                                        response_cache=response_cache)
     cost_ordered_idents = sorted(costs.keys(), key=lambda ident: costs[ident])
-    pprint([{"cost": f"{costs[ident]:.1f}", "ident": ident} for ident in cost_ordered_idents])
+    pprint([{"cost": costs[ident], "ident": ident} for ident in cost_ordered_idents])
 
     cost_ordered_idents_screened = []
     for ident in cost_ordered_idents:

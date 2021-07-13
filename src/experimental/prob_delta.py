32e6c7dd19208c38cfe189ebb06dae818def5795
nostalgebraist <nostalgebraist@gmail.com>
2021-07-12 19:10:44 -0700

    needs_empty_reblog flag

--- src/experimental/prob_delta.py
+++ src/experimental/prob_delta.py
@@ -7,8 +7,9 @@ from tumblr_to_text.nwo_munging import add_empty_reblog
 from api_ml.ml_connector import prob_delta_from_gpt
 
 
-def construct_prob_delta_prompts(thread: TumblrThread):
-    thread = add_empty_reblog(thread, 'DUMMYUSER', datetime.now())
+def construct_prob_delta_prompts(thread: TumblrThread, needs_empty_reblog=True):
+    if needs_empty_reblog:
+        thread = add_empty_reblog(thread, 'DUMMYUSER', datetime.now())
 
     prompt = npf_thread_to_formatted_text(thread, prob_delta_format=True)
 
@@ -20,14 +21,15 @@ def construct_prob_delta_prompts(thread: TumblrThread):
     return prompt, prompt_ref, forbidden_strings
 
 
-def get_prob_delta_for_payloads(post_payloads: list, blog_name: str):
+def get_prob_delta_for_payloads(post_payloads: list, blog_name: str, needs_empty_reblog=True):
     token_str = " " + blog_name
     kwargs = {"text": [], "text_ref": [], "token_str": token_str, "forbidden_strings": []}
 
     for pp in post_payloads:
         thread = TumblrThread.from_payload(pp)
 
-        text, text_ref, forbidden_strings = construct_prob_delta_prompts(thread)
+        text, text_ref, forbidden_strings = construct_prob_delta_prompts(thread,
+                                                                         needs_empty_reblog=needs_empty_reblog)
         kwargs["text"].append(text)
         kwargs["text_ref"].append(text_ref)
         kwargs["forbidden_strings"].append(forbidden_strings)

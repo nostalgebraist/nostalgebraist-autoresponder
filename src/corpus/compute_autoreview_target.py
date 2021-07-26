279067af958cb3d3c468c37465013233d660e5f1
nostalgebraist <nostalgebraist@gmail.com>
2021-07-26 11:52:42 -0700

    finished 1st attempt

--- src/corpus/compute_autoreview_target.py
+++ src/corpus/compute_autoreview_target.py
@@ -1,13 +1,30 @@
 import json
 import argparse
 from collections import defaultdict
+from datetime import datetime
+
+from tqdm.auto import tqdm
 
 from persistence import traceability
+from corpus.blog_archive import roll_head_timestamp
+
+
+def sub_prompt_timestamp(base_head_timestamp, prompt_autoreview):
+    before, sep, seg = prompt_autoreview.rpartition("\n\n Written")
+    timeseg, sep2, after = seg.partition(" | ")
+
+    ts = datetime.strptime(timeseg, "%-I %p %B %Y")
+    head_ts = roll_head_timestamp(base_head_timestamp=base_head_timestamp, actual_timestamp=ts)
+
+    return before + sep + head_ts.strftime("%-I %p %B %Y") + sep2 + after
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--dryrun", action="store_true")
     args = parser.parse_args()
+
+    base_head_timestamp = datetime.now()
 
     # trace
     print("loading trace logs")
@@ -19,6 +36,16 @@ def main():
                   if row.get("requested__state") == "draft"]
 
     print(f"subsetted trace logs to draft:  {len(trace_logs)} rows")
+
+    trace_logs = [row for row in trace_logs
+                  if "prompt_autoreview" in row and "choice_ix" in row and "all_continuations" in row]
+
+    print(f"subsetted trace logs to nwo / usable:  {len(trace_logs)} rows")
+
+    trace_indices_to_texts = {}
+    for i, row in enumerate(trace_logs):
+        subbed = sub_prompt_timestamp(base_head_timestamp, row["prompt_autoreview"])
+        trace_indices_to_texts[i] = subbed + row["all_continuations"][row["choice_ix"]]
 
     trace_map = defaultdict(list)
 
@@ -45,9 +72,66 @@ def main():
     print('matching...')
 
     trace_indices_to_targets = {}
+    trace_indices_to_texts = {}
+    trace_indices_to_published_ids = {}
 
-    for api__id, group_trace_indices in trace_map.items():
-        pass  # TODO
+    n_accept = 0
+    n_reject = 0
+    n_multimatch = 0
+
+    iter_ = tqdm(trace_map.items(), total=len(trace_map), mininterval=1, smoothing=0)
+
+    for api__id, group_trace_indices in iter_:
+        pub_gids_matching_trace_id = pub_map.get(api__id, [])
+
+        if len(pub_gids_matching_trace_id) == 0:
+            # never published
+            for trace_index in group_trace_indices:
+                trace_indices_to_targets[trace_index] = 'reject'
+                trace_indices_to_published_ids[trace_index] = None
+            n_reject += len(group_trace_indices)
+        else:
+            if len(pub_gids_matching_trace_id) > 1:
+                # ???
+                n_multimatch += 1
+
+            matching_pub_row = pub_gids_matching_trace_id[0]
+
+            # assumes trace is ordered by time -- i believe this is true
+            pubd_ix = group_trace_indices[-1]
+            trace_indices_to_targets[pubd_ix] = 'accept'
+            trace_indices_to_published_ids[pubd_ix] = matching_pub_row["id"]
+
+            for trace_index in group_trace_indices[:-1]:
+                trace_indices_to_targets[trace_index] = 'reject'
+                trace_indices_to_published_ids[trace_index] = None
+
+            n_accept += 1
+            n_reject += len(group_trace_indices) - 1
+
+        iter_.set_postfix(n_accept=n_accept, n_reject=n_reject, zz_n_multimatch=n_multimatch)
+
+    # verify
+    n_accept_verify = sum(v == 'accept' for v in trace_indices_to_targets.values())
+    n_reject_verify = sum(v == 'reject' for v in trace_indices_to_targets.values())
+
+    print(f'\nn_accept: {n_accept_verify} vs {n_accept}')
+    print(f'n_reject: {n_reject_verify} vs {n_reject}')
+
+    autoreview_train_data = []
+    for ix in sorted(trace_indices_to_targets.keys()):
+        autoreview_train_data.append(
+            {
+                "text": trace_indices_to_texts[ix],
+                "target": trace_indices_to_targets[ix],
+                "trace_api__id": trace_logs[ix]["api__id"],
+                "pub_api__id": trace_indices_to_published_ids[ix]
+            }
+        )
+
+    if not args.dryrun:
+        with open("autoreview_train_data.json", "w", encoding="utf-8") as f:
+            json.dump(autoreview_train_data, f)
 
 
 if __name__ == "__main__":

389442d2d6a8d58cfb0241da8006ca016be76324
nostalgebraist <nostalgebraist@gmail.com>
2021-12-18 10:37:38 -0800

    add utility

--- src/multimodal/image_analysis_static.py
+++ src/multimodal/image_analysis_static.py
@@ -1,3 +1,5 @@
+import re
+
 IMAGE_DIR = "data/analysis_images/"
 
 ACCEPTABLE_IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif"}
@@ -7,6 +9,8 @@ AR_DETECT_TEXT_CONFIDENCE_THRESHOLD = 95
 IMAGE_DELIMITER = "======="
 IMAGE_DELIMITER_WHITESPACED = "\n=======\n"
 
+extract_image_text_regex = re.compile(r"=======\n(.*?)\n=======", flags=re.DOTALL)
+
 
 def PRE_V9_IMAGE_FORMATTER(image_text):
     return "\n" + image_text + "\n"
@@ -14,3 +18,7 @@ def PRE_V9_IMAGE_FORMATTER(image_text):
 
 def V9_IMAGE_FORMATTER(image_text):
     return "\n" + IMAGE_DELIMITER_WHITESPACED + image_text + IMAGE_DELIMITER_WHITESPACED
+
+
+def extract_image_texts_from_post_text(s):
+    return extract_image_text_regex.findall(s)

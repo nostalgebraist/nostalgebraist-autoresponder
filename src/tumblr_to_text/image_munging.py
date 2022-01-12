61a87eb658a8989095fdeea3b8dcf1de48324304
nostalgebraist <nostalgebraist@gmail.com>
2022-01-11 22:15:02 -0800

    whoops

--- src/tumblr_to_text/image_munging.py
+++ src/tumblr_to_text/image_munging.py
@@ -105,7 +105,7 @@ def find_text_images_and_sub_real_images(
     escaped_delim = IMAGE_DELIMITER.encode('unicode_escape').decode()
     escaped_delim_ws = IMAGE_DELIMITER_WHITESPACED.encode('unicode_escape').decode()
     imtext_regex = rf"({escaped_delim_ws})(.+?)({escaped_delim}\n)"
-    figure_format = """<figure data-orig-height="{h}" data-orig-width="{w}"><img src="{url}" data-orig-height="{h}" data-orig-width="{w}" alt={alt}/></figure>"""
+    figure_format = """<figure data-orig-height="{h}" data-orig-width="{w}"><img src="{url}" data-orig-height="{h}" data-orig-width="{w}" alt="{alt}"/></figure>"""
     imtexts = []
     imtext_positions = []
     ims_checksum = 0

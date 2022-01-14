cc0b5e2f7e4b15f28971d2de903e2a770f89e77b
nostalgebraist <nostalgebraist@gmail.com>
2022-01-14 08:50:35 -0800

    tweak guidance scale options

--- src/tumbl.py
+++ src/tumbl.py
@@ -189,7 +189,7 @@ FIC_TRIGGER_TESTING = False
 IMAGE_CREATION = True
 IMAGE_CREATION_TESTING = False
 IMAGE_CREATION_DIFFUSION = True
-GUIDANCE_SCALE_OPTIONS = (1, 1, 2, 2, 2, 3, 3, 3, 4)
+GUIDANCE_SCALE_OPTIONS = (0, 1, 1, 1, 2, 2, 3,)
 
 with open("data/scraped_usernames.json", "r") as f:
     scraped_usernames = json.load(f)

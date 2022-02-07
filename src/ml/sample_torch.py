4236630993cc09c063c6e3a0d9dfb6e9823abbe5
nostalgebraist <nostalgebraist@gmail.com>
2022-02-07 09:03:54 -0800

    apply bugfix to typical sampling - cf https://twitter.com/clara__meister/status/1490636382443819009

--- src/ml/sample_torch.py
+++ src/ml/sample_torch.py
@@ -54,7 +54,7 @@ class BreakrunsLogitsProcessor(LogitsProcessor):
         return scores / eff_temperature
 
 
-# taken from https://github.com/cimeister/typical-sampling/blob/0f24c9409dc078ed23982197e8af1439093eedd3/src/transformers/generation_logits_process.py
+# taken from https://github.com/cimeister/typical-sampling/blob/typical-pr/src/transformers/generation_logits_process.py
 # implements method from https://arxiv.org/abs/2202.00666
 class TypicalLogitsWarper(LogitsWarper):
     def __init__(self, mass: float = 0.9, filter_value: float = -float("Inf"), min_tokens_to_keep: int = 1):
@@ -68,27 +68,22 @@ class TypicalLogitsWarper(LogitsWarper):
         # calculate entropy
         normalized = torch.nn.functional.log_softmax(scores, dim=-1)
         p = torch.exp(normalized)
-        ent = -(normalized * p).sum(-1, keepdim=True)
+        ent = -(normalized * p).nansum(-1, keepdim=True)
 
         # shift and sort
-        shifted_scores = torch.abs((-ent) - normalized)
-        _, sorted_indices = torch.sort(shifted_scores, descending=False)
+        shifted_scores = torch.abs((-normalized) - ent)
+        sorted_scores, sorted_indices = torch.sort(shifted_scores, descending=False)
         sorted_logits = scores.gather(-1, sorted_indices)
         cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
 
-        # Remove tokens with cumulative top_p above the threshold (token with 0 are kept)
-        sorted_indices_to_remove = cumulative_probs > self.mass
+        # Remove tokens with cumulative mass above the threshold
+        last_ind = (cumulative_probs < self.mass).sum(dim=1)
+        last_ind[last_ind < 0] = 0
+        sorted_indices_to_remove = sorted_scores > sorted_scores.gather(1, last_ind.view(-1, 1))
         if self.min_tokens_to_keep > 1:
             # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
-            sorted_indices_to_remove[..., : self.min_tokens_to_keep - 1] = 0
-        # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[...,
-                                 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
-
-        # scatter sorted tensors to original indexing
-        indices_to_remove = sorted_indices_to_remove.scatter(
-            1, sorted_indices, sorted_indices_to_remove)
+            sorted_indices_to_remove[..., : self.min_tokens_to_keep] = 0
+        indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
 
         scores = scores.masked_fill(indices_to_remove, self.filter_value)
         return scores

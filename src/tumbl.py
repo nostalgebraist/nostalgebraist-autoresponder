649d004a5f6b9934b638bc8b77ac22538495b861
nostalgebraist <nostalgebraist@gmail.com>
2022-02-22 12:35:36 -0800

    decrease image scraping speed

--- src/tumbl.py
+++ src/tumbl.py
@@ -1323,10 +1323,10 @@ def is_statically_reblog_worthy_on_dash(
     if '.gif' in p_body:
         scrape_worthy = False
 
-    if n_img > 8:
+    if n_img > 3:
         scrape_worthy = False
 
-    if n_img > 0 and random.random() > 1.:  # disabled
+    if n_img > 0 and random.random() > 0.5:
         scrape_worthy = False
 
     if (not slow_scraping_ok) and (n_img > 0):

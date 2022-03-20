8957b18f92d1a42479234f708616306d1e201c01
nostalgebraist <nostalgebraist@gmail.com>
2022-03-20 09:06:40 -0700

    lower n_posts_to_check_base

--- src/tumbl.py
+++ src/tumbl.py
@@ -805,7 +805,7 @@ class LoopPersistentData:
         reply_metadata={},
         timestamps={},
         reblog_keys={},
-        n_posts_to_check_base=250,
+        n_posts_to_check_base=150,
         n_posts_to_check_dash=690,
         n_notifications_to_check=1000,
         offset_=0,

f86d0e5dbb2cb152833147acb4b340055b83e890
nostalgebraist <nostalgebraist@gmail.com>
2022-03-20 14:10:40 -0700

    resilience

--- src/util/cloudsave.py
+++ src/util/cloudsave.py
@@ -22,12 +22,16 @@ def resilient_pickle_save(obj, path, backup_path):
             with open(path, "wb") as f:
                 pickle.dump(obj, f)
             done = True
-        except smart_open.gcs.UploadFailedError:
+        except Exception as e:
             # TODO: delete the object in gcs programmatically here.
             # as is, it will probably fail endlessly b/c it's partway through a multipart upload
             # however, at least that keeps the obj in memory until i can manually delete it
             sleeptime = 2**tries
             print(f"gcs upload failed for {path}. {tries} tries so far.  sleeping {sleeptime}s")
+            try:
+                print(f"exception: {e}, {e.args}")
+            except:
+                pass
             if tries == 0:
                 print(f"saving backup to {backup_path}s")
                 with open(backup_path, "wb") as f:
@@ -46,9 +50,13 @@ def resilient_pickle_load(path):
             with open(path, "rb") as f:
                 obj = pickle.load(f)
             done = True
-        except smart_open.gcs.UploadFailedError:
+        except Exception as e:
             sleeptime = 2**tries
             print(f"gcs download failed for {path}. {tries} tries so far.  sleeping {sleeptime}s")
+            try:
+                print(f"exception: {e}, {e.args}")
+            except:
+                pass
             time.sleep(sleeptime)
             tries += 1
     return obj

f7836f46db767d94aca175abb9c71a51df66af6d
nostalgebraist <nostalgebraist@gmail.com>
2022-05-14 10:08:46 -0700

    tweak diffusion params

--- src/api_ml/ml_layer_diffusion.py
+++ src/api_ml/ml_layer_diffusion.py
@@ -26,8 +26,8 @@ BRIDGE_SERVICE_REMOTE_HOST = bot_specific_constants.BRIDGE_SERVICE_REMOTE_HOST
 HF_REPO_NAME_DIFFUSION = 'nostalgebraist/nostalgebraist-autoresponder-diffusion'
 model_path_diffusion = 'nostalgebraist-autoresponder-diffusion'
 timestep_respacing_sres1 = '90,60,60,20,20'
-timestep_respacing_sres2 = '90,60,60,20,20'
-timestep_respacing_sres3 = '90,60,60,20,20'
+timestep_respacing_sres2 = '150,50,25,25'
+timestep_respacing_sres3 = '150,50,25,25'
 
 DIFFUSION_DEFAULTS = dict(
     batch_size=2,

b97dc16ed0209a8fffb6d24a54841758fe682926
nostalgebraist <nostalgebraist@gmail.com>
2022-05-28 18:37:15 -0700

    fewer posts per dash check

--- src/tumbl.py
+++ src/tumbl.py
@@ -818,7 +818,7 @@ class LoopPersistentData:
         timestamps={},
         reblog_keys={},
         n_posts_to_check_base=250,
-        n_posts_to_check_dash=350,
+        n_posts_to_check_dash=200,
         n_notifications_to_check=200,
         offset_=0,
         requests_per_check_history_private=[],

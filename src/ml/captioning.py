7f7e6b36dbe2b7ecdf1a8b714a40238cef74af3d
nostalgebraist <nostalgebraist@gmail.com>
2022-06-23 23:06:21 -0700

    whoops

---
+++ src/ml/captioning.py
@@ -0,0 +1,58 @@
+from PIL import Image
+from magma import Magma, ImageInput
+from magma.sampling import generate_cfg
+
+from util.error_handling import LogExceptionAndSkip
+
+
+def caption_image_from_url(url: str, magma_wrapper: Magma):
+    frames = url_to_frame_bytes(url)
+
+    if len(frames) != 1:
+        return
+
+    im = Image.open(BytesIO(frames[0]))
+
+    return caption_image(im)
+
+
+def caption_image(
+    path_or_url: str,
+    magma_wrapper: Magma,
+    temperature=1.0,
+    top_p=0.5,
+    top_k=0,
+    max_steps=30,
+    guidance_scale=0,
+):
+    # todo: control sampling params
+
+    inputs =[
+        ## supports urls and path/to/image
+        ImageInput(path_or_url),
+        '[Image description:',  # todo: precompute embeds for this
+    ]
+
+    magma_wrapper.add_adapters()
+
+    caption = None
+
+    with LogExceptionAndSkip('trying to caption image'):
+        embeddings = model.preprocess_inputs(inputs)
+
+        output = generate_cfg(
+            model=magma_wrapper,
+            embeddings=embeddings,
+            max_steps = 6,
+            temperature=temperature,
+            top_k=top_k,
+            top_p=top_p,
+            max_steps=max_steps,
+            gs=guidance_scale,
+        )
+
+        caption = output[0]
+
+    magma_wrapper.detach_adapters()
+
+    return caption

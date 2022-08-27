7eb719dbce801d359ff2a45c4d5fe2a64424ffb2
nostalgebraist <nostalgebraist@gmail.com>
2022-08-26 17:29:03 -0700

    forgot to add

---
+++ src/corpus/prob_delt_archive.py
@@ -0,0 +1,9 @@
+import jsonlines
+
+
+PROB_DELT_ARCHIVE_PATH = "data/prob_delt_archive.jsonl"
+
+
+def archive_prob_delt(kind, user, substring,, prob_delt, path=PROB_DELT_ARCHIVE_PATH):
+    with jsonlines.open(path, mode='a') as f:
+        f.write(dict(kind=kind, user=user, substring=substring, prob_delt=float(prob_delt)}))

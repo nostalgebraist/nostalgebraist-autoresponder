c04da2ca3f38505c144426a1f31468434f145e3b
nostalgebraist <nostalgebraist@gmail.com>
2022-08-26 17:06:13 -0700

    propagate

--- src/experimental/prob_delta.py
+++ src/experimental/prob_delta.py
@@ -26,7 +26,7 @@ def construct_prob_delta_prompts(thread: TumblrThread, needs_empty_reblog=True, 
     return prompt, prompt_ref, forbidden_strings
 
 
-def get_prob_delta_for_payloads(post_payloads: list, blog_name: str, needs_empty_reblog=True):
+def get_prob_delta_for_payloads(post_payloads: list, blog_name: str, needs_empty_reblog=True, skip_asking_name=False):
     token_str = " " + blog_name
     kwargs = {"text": [], "text_ref": [], "token_str": token_str, "forbidden_strings": []}
 
@@ -34,7 +34,8 @@ def get_prob_delta_for_payloads(post_payloads: list, blog_name: str, needs_empty
         thread = TumblrThread.from_payload(pp)
 
         text, text_ref, forbidden_strings = construct_prob_delta_prompts(thread,
-                                                                         needs_empty_reblog=needs_empty_reblog)
+                                                                         needs_empty_reblog=needs_empty_reblog,
+                                                                         skip_asking_name=skip_asking_name)
         kwargs["text"].append(text)
         kwargs["text_ref"].append(text_ref)
         kwargs["forbidden_strings"].append(forbidden_strings)

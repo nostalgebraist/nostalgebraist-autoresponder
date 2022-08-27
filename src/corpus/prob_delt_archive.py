7af22d9bf9dd11ca16771a16e6ad87135fbca804
nostalgebraist <nostalgebraist@gmail.com>
2022-08-26 17:30:05 -0700

    fix

--- src/corpus/prob_delt_archive.py
+++ src/corpus/prob_delt_archive.py
@@ -4,6 +4,6 @@ import jsonlines
 PROB_DELT_ARCHIVE_PATH = "data/prob_delt_archive.jsonl"
 
 
-def archive_prob_delt(kind, user, substring,, prob_delt, path=PROB_DELT_ARCHIVE_PATH):
+def archive_prob_delt(kind, user, substring, prob_delt, path=PROB_DELT_ARCHIVE_PATH):
     with jsonlines.open(path, mode='a') as f:
         f.write(dict(kind=kind, user=user, substring=substring, prob_delt=float(prob_delt)}))

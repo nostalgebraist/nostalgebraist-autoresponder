a9606072fd9c2e9b356450a294ef0101e5d61f97
nostalgebraist <nostalgebraist@gmail.com>
2022-08-27 10:44:53 -0700

    adaptive stop-above-cost

--- src/tumbl.py
+++ src/tumbl.py
@@ -2338,12 +2338,14 @@ def do_reblog_reply_handling(
     cost_ordered_idents = sorted(costs.keys(), key=lambda ident: costs[ident])
     pprint([{"cost": costs[ident], "ident": ident} for ident in cost_ordered_idents])
 
+    effective_stop_above_cost = STOP_ABOVE_COST + loop_persistent_data.slowdown_level['STOP_ABOVE_COST_modifier']
+
     cost_ordered_idents_screened = []
     for ident in cost_ordered_idents:
         if costs[ident] < STOP_ABOVE_COST:
             cost_ordered_idents_screened.append(ident)
         else:
-            print(f"ignoring {ident} forever with cost {costs[ident]:.1f}!")
+            print(f"ignoring {ident}: cost {costs[ident]:.1f} >= effective_stop_above_cost {effective_stop_above_cost:.1f}")
     cost_ordered_idents = cost_ordered_idents_screened
 
     blog_names_to_idents = defaultdict(list)

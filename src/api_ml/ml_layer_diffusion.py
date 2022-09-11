3f1d053f5414da89e4927740f601c943e114715c
nostalgebraist <nostalgebraist@gmail.com>
2022-09-11 14:26:09 -0700

    plms back on with fix in other repo

--- src/api_ml/ml_layer_diffusion.py
+++ src/api_ml/ml_layer_diffusion.py
@@ -31,13 +31,13 @@ BRIDGE_SERVICE_REMOTE_HOST = bot_specific_constants.BRIDGE_SERVICE_REMOTE_HOST
 HF_REPO_NAME_DIFFUSION = 'nostalgebraist/nostalgebraist-autoresponder-diffusion-captions'
 model_path_diffusion = 'nostalgebraist-autoresponder-diffusion'
 
-USE_PLMS = False
+USE_PLMS = True
 
 if USE_PLMS:
-    timestep_respacing_sres1 = '100'
-    timestep_respacing_sres1p5 = '100'
-    timestep_respacing_sres2 = '90,60,60,20,20'
-    timestep_respacing_sres3 = '90,60,60,20,20'
+    timestep_respacing_sres1 = 'ddim50'
+    timestep_respacing_sres1p5 = 'ddim50'
+    timestep_respacing_sres2 = 'ddim50'
+    timestep_respacing_sres3 = 'ddim50'
 else:
     timestep_respacing_sres1 = '250'
     timestep_respacing_sres1p5 = '90,60,60,20,20'
@@ -226,7 +226,7 @@ def poll(
             guidance_scale=guidance_scale_step2,
             noise_cond_ts=150,
             dynamic_threshold_p=data.get('dynamic_threshold_p', 0.995),
-            # use_plms=USE_PLMS,
+            use_plms=USE_PLMS,
         )
 
         print('step2 done')
@@ -248,7 +248,7 @@ def poll(
                 low_res=result,
                 noise_cond_ts=75,
                 dynamic_threshold_p=data.get('dynamic_threshold_p', 0.995),
-                # use_plms=USE_PLMS,
+                use_plms=USE_PLMS,
             )
 
             # sampling_model_sres3.model.cpu();

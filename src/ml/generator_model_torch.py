0f81d3980f6e5f729c3c307a4ed908b47324d7a6
nostalgebraist <nostalgebraist@gmail.com>
2022-10-22 17:04:36 -0700

    use logprob in prob delt calculation

--- src/ml/generator_model_torch.py
+++ src/ml/generator_model_torch.py
@@ -379,7 +379,8 @@ class GeneratorModelTorch:
         return probs
 
     def get_prob_delta_over_ref(self, text: str, text_ref: str, token_str: str,
-                                forbidden_strings: List[str]):
+                                forbidden_strings: List[str],
+                                use_logprobs=True):
         if token_str in forbidden_strings:
             return 0.
 
@@ -390,7 +391,7 @@ class GeneratorModelTorch:
         prob_ref = self.get_next_probs(text_ref, forbidden_tokens=[], to_numpy=True)[token]
         prob = self.get_next_probs(text, forbidden_tokens=forbidden_tokens, to_numpy=True)[token]
 
-        delta = prob - prob_ref
+        delta = np.log(prob) - np.log(prob_ref)
         delta = float(delta)
         return delta
 

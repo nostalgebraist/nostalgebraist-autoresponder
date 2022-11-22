b9c8b40b7cbeaaf7e236f5b08908a3f78d7d8214
nostalgebraist <nostalgebraist@gmail.com>
2022-11-22 13:24:56 -0800

    bump sentiment version

--- src/config/autoresponder_config.py
+++ src/config/autoresponder_config.py
@@ -267,7 +267,7 @@ ckpt_captioner = None
 
 if V12_19:
     ckpt_select = "selector/v12_19/v4_experimental/"
-    ckpt_sentiment = "sentiment/v12_19/v1/"
+    ckpt_sentiment = "sentiment/v12_19/v2/"
     ckpt_autoreviewer = "draft_autoreviewer/v12_19/v4_experimental/"
     ckpt_captioner = "captioner/v12_19/v1/"
 elif V12_18:

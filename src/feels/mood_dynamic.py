20da3b8e94616add4fa53ce38bfefb6947a72887
nostalgebraist <nostalgebraist@gmail.com>
2022-11-22 08:26:18 -0800

    mood update

--- src/feels/mood_dynamic.py
+++ src/feels/mood_dynamic.py
@@ -122,6 +122,7 @@ DETERMINER_CENTER_UPDATES = {
     pd.Timestamp("2022-08-21 23:30:00"): 0.05,
     pd.Timestamp("2022-10-22 10:40:00"): 0.,
     pd.Timestamp("2022-11-17 08:00:00"): 0.05,
+    pd.Timestamp("2022-11-22 00:08:30"): 0.1,
 }
 DETERMINER_MULTIPLIER_UPDATES = {
     pd.Timestamp("2020-08-25 17:00:00"): 0.1 / RESPONSE_SCALE_BASE,

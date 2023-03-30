0959892651965305c31abea31fc9ac398020d143
nostalgebraist <nostalgebraist@gmail.com>
2023-03-30 08:13:32 -0700

    mood update

--- src/feels/mood_dynamic.py
+++ src/feels/mood_dynamic.py
@@ -150,6 +150,7 @@ DETERMINER_CENTER_UPDATES = {
     pd.Timestamp("2023-03-24 14:30:00"): 0.04,
     pd.Timestamp("2023-03-25 16:45:00"): 0.1,
     pd.Timestamp("2023-03-29 08:35:00"): 0.075,
+    pd.Timestamp("2023-03-30 08:15:00"): 0.05,
 }
 DETERMINER_MULTIPLIER_UPDATES = {
     pd.Timestamp("2020-08-25 17:00:00"): 0.1 / RESPONSE_SCALE_BASE,

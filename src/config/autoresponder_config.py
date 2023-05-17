65ccc6c784b1d316b6139317186a7b248b4db7ec
nostalgebraist <nostalgebraist@gmail.com>
2023-05-17 11:37:20 -0700

    remove captioning from llama machines

--- src/config/autoresponder_config.py
+++ src/config/autoresponder_config.py
@@ -712,7 +712,6 @@ if V12_16:
 if COCA_CAPTIONING:
     MODELS_SERVED_LEGACY.remove("captioner")
     MODELS_SERVED_LEGACY.add("captioner_coca")
-    MODELS_SERVED_LLAMA.add("captioner_coca")
 
 # "all", "only_write", "only_write_prob_delt", "all_except_write", "all_except_write_prob_delt"
 if LLAMA_PROB_DELT:
